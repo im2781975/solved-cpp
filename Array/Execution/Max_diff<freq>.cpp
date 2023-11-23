e2ff3cc814429f65675ea3939af9fc6b70fc4a69
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-23 17:42:14 +0100

    Update and rename Max_diff<0> to Max_diff<freq>.cpp

--- Array/Execution/Max_diff<freq>.cpp
+++ Array/Execution/Max_diff<freq>.cpp
@@ -1,18 +1,11 @@
-// Efficient C++ program to find maximum 
-// difference between frequency of any two 
-// elements such that element with greater 
-// frequency is also greater in value. 
+//find maximum diff between frequency of any two 
+// elements such that element with greater freq is also greater in value. 
 #include<bits/stdc++.h> 
 using namespace std; 
  
-// Return the maximum difference between 
-// frequencies of any two elements such that 
-// element with greater frequency is also 
-// greater in value. 
 int maxdiff(int arr[], int n) 
 { 
     unordered_map<int, int> freq; 
- 
     int dist[n]; 
  
     // Finding the frequency of each element. 
@@ -24,16 +17,12 @@ int maxdiff(int arr[], int n)
  
         freq[arr[i]]++; 
     } 
- 
     // Sorting the distinct element 
     sort(dist, dist + j); 
  
     int min_freq = n+1; 
  
-    // Iterate through all sorted distinct elements. 
-    // For each distinct element, maintaining the 
-    // element with minimum frequency than that 
-    // element and also finding the maximum 
+    //Iterate through all sorted distinct elements For each distinct element, maintaining the element with minimum frequency than that element and also finding the maximum 
     // frequency difference 
     int ans = 0; 
     for (int i=0; i<j; i++) 
@@ -42,11 +31,8 @@ int maxdiff(int arr[], int n)
         ans = max(ans, cur_freq - min_freq); 
         min_freq = min(min_freq, cur_freq); 
     } 
- 
     return ans; 
 } 
- 
-// Driven Program 
 int main() 
 { 
     int arr[] = { 3, 1, 3, 2, 3, 2 }; 
