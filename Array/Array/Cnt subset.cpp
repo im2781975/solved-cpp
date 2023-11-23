a98e438aa28a78611818e89d9ee0ac94ed67965b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-23 11:59:10 +0100

    Update and rename Cnt min subset to Cnt subset.cpp

--- Array/Array/Cnt subset.cpp
+++ Array/Array/Cnt subset.cpp
@@ -1,33 +1,32 @@
-// C++ program to find number of subset containing 
-// consecutive numbers 
+// find number of subset containing consecutive numbers 
 #include <bits/stdc++.h> 
 using namespace std; 
   
 // Returns count of subsets with consecutive numbers 
 int numofsubset(int arr[], int n) 
 { 
-    // Sort the array so that elements which are 
-    // consecutive in nature became consecutive 
-    // in the array. 
     sort(arr, arr + n); 
   
-    int count = 1; // Initialize result 
+    int count = 1; 
     for (int i = 0; i < n - 1; i++) { 
         // Check if there is beginning of another 
         // subset of consecutive number 
         if (arr[i] + 1 != arr[i + 1]) 
             count++; 
     } 
-  
     return count; 
 } 
-  
-// Driven Program 
 int main() 
 { 
-    int arr[] = { 100, 56, 5, 6, 102, 58, 101, 
-                  57, 7, 103, 59 }; 
+    int arr[] = { 100, 56, 5, 6, 102, 58, 101, 57, 7, 103, 59 }; 
     int n = sizeof(arr) / sizeof(arr[0]); 
-    cout << numofsubset(arr, n) << endl; 
+    sort(arr,arr+n);
+    
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n";
+    cout << numofsubset(arr, n) ;
     return 0; 
 } 
