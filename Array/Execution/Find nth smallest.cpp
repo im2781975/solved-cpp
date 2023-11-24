86d93c143285dbf9b15123376bc3ea6fecd23f47
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-24 11:33:34 +0100

    Update Find nth smallest.cpp

--- Array/Execution/Find nth smallest.cpp
+++ Array/Execution/Find nth smallest.cpp
@@ -1,32 +1,32 @@
-//find the K-th smallest element
-// after removing some integers from natural number.
-#include <bits/stdc++.h>
+//find the K-th smallest element after removing some integers from natural number.
+#include <iostream>
+#include <cstring>
 #define MAX 1000000
+
 using namespace std;
- 
 int ksmallest(int arr[], int n, int k)
 {
-    int b[MAX];
+    int b[MAX + 1]; 
     memset(b, 0, sizeof b);
- 
+
     for (int i = 0; i < n; i++)
         b[arr[i]] = 1;
- 
-    for (int j = 1; j < MAX; j++) {
-        // If j is unmarked, reduce k by 1.
+
+    for (int j = 1; j <= MAX; j++) { 
+       // If j is unmarked, reduce k by 1.
         if (b[j] != 1)
             k--;
- 
-        // If k is 0 return j.
-        if (!k)
+        //After decrementing k, it checks whether k has reached 0. If k is 0, it means that the current value of j is the K-th smallest element after removing some integers. In this case, the function returns this value (j).
+        if (k == 0)
             return j;
     }
+    return -1;
 }
 int main()
 {
     int k = 1;
-    int arr[] = { 1 };
+    int arr[] = {1};
     int n = sizeof(arr) / sizeof(arr[0]);
-    cout << ksmallest(arr, n, k);
+    cout << ksmallest(arr, n, k) << endl;
     return 0;
 }
