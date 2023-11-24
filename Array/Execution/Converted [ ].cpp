17d691a6173db7ef1332f8407cefd6ee33b72229
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-24 18:14:32 +0100

    Update Converted [ ].cpp

--- Array/Execution/Converted [ ].cpp
+++ Array/Execution/Converted [ ].cpp
@@ -2,11 +2,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void convert_arr(int arr[], int n)
+{
+    int sorted_arr[n];
+    copy(arr, arr+n, sorted_arr);
+ 
+    sort(sorted_arr, sorted_arr+n);
+ 
+    // Map each element of the sorted array to its index in the range 0 to n-1
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++) {
+        mp[sorted_arr[i]] = i;
+    }
+ 
+    // Replace each element of the input array with its corresponding index
+    for (int i = 0; i < n; i++) {
+        arr[i] = mp[arr[i]];
+    }
+}
 void convert(int arr[], int n)
 {
     vector<pair<int, int> > v;
  
-    for (int i = 0; i < n; ++)
+    for (int i = 0; i < n; i++)
         v.push_back(make_pair(arr[i], i));
  
     sort(v.begin(), v.end());
@@ -28,7 +46,8 @@ int main()
     cout << "\nArray is: ";
     printArr(arr, n);
  
-    convert(arr, n);
+    //convert(arr, n);
+    convert_arr(arr,n);
  
     cout<<"\nConverted Array is: ";
     printArr(arr, n);
