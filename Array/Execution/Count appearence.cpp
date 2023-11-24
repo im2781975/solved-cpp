12dcf5982a7bba6410f519f02860992a7cee2175
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-24 12:47:10 +0100

    Update and rename Count distinct to Count appearence.cpp

--- Array/Execution/Count appearence.cpp
+++ Array/Execution/Count appearence.cpp
@@ -2,38 +2,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Recursive function with memoization
 int f(int i, int j, string s, string t, vector<vector<int>>& dp) {
     // If remaining characters in t are less than remaining characters in s, not possible to form a subsequence
-    if (t.size() - j > s.size() - i)
+    if (t.size() - j > s.size() -i)
         return 0;
-
-    // Base case: If we have reached the end of string t, we have found a valid subsequence
+    //If we have reached the end of string t, we have found a valid subsequence
     if (j == t.size()) {
         return 1;
     }
-
-    // Base case: If we have reached the end of string s, but not the end of t, return 0
+    // If we have reached the end of string s, but not the end of t, return 0
     if (i == s.size()) {
         return 0;
     }
-
-    // Memoization: If the result for the current indices is already computed, return that value
+    //If the result for the current indices is already computed, return that value
     if (dp[i][j] != -1) {
         return dp[i][j];
     }
-
     // If the characters at the current indices match
-    if (s[i] == t[j]) {
-        // Recursively call f for the next indices in both s and t
+    if (s[i] == t[j]) 
+    {
+        //Recursively call f for the next indices in both s and t
         // Add the counts of subsequences including and excluding the current character in s
         return dp[i][j] = f(i + 1, j + 1, s, t, dp) + f(i + 1, j, s, t, dp);
     }
-
     // If the characters don't match, only exclude the current character from s and move to the next index in s
     return dp[i][j] = f(i + 1, j, s, t, dp);
 }
-
 int findSubsequenceCount(string s, string t) {
 
     vector<vector<int>> dp(s.size(), vector<int>(t.size(), -1));
