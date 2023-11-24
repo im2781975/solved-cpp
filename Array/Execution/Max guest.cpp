2a4fb3e57ace1e13d9b01dbe2819cb7fb06ced18
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-24 15:22:36 +0100

    Update Max guest.cpp

--- Array/Execution/Max guest.cpp
+++ Array/Execution/Max guest.cpp
@@ -1,38 +1,36 @@
-//find the maximum number of guests at any time in a party
+//find the maximum number of guests present at any given time. .arrl[] and exit[], representing the arrival and departure times of guests.
 #include<iostream>
 #include<algorithm>
 using namespace std;
-
+ 
 void findMaxGuests(int arrl[], int exit[], int n)
 {
    // Sort arrival and exit arrays
    sort(arrl, arrl+n);
    sort(exit, exit+n);
-
-   // guests_in indicates the number of guests at a given time
+ 
    int guests_in = 1, max_guests = 1, time = arrl[0];
    int i = 1, j = 0;
-
-   // Loop to process all events in sorted order
+ 
+   // Similar to merge in merge sort to process all events in sorted order
    while (i < n && j < n)
    {
-      // If the next event in sorted order is an arrival,
-      // increment the count of guests
+      // If next event in sorted order is arrival,
+      // increment count of guests
       if (arrl[i] <= exit[j])
       {
           guests_in++;
-
-          // Update max_guests and time if needed
+ 
+          // Update max_guests if needed
           if (guests_in > max_guests)
           {
               max_guests = guests_in;
               time = arrl[i];
           }
-
-          i++;  // Increment index of the arrival array
+          i++;  //increment index of arrival array
       }
-      else // If the event is an exit, decrement the count of guests
-      {
+      else // If event is exit, decrement count
+      {    // of guests.
           guests_in--;
           j++;
       }
@@ -45,7 +43,6 @@ int main()
     int arrl[] = {1, 2, 10, 5, 5};
     int exit[] = {4, 5, 12, 9, 12};
     int n = sizeof(arrl)/sizeof(arrl[0]);
-
     findMaxGuests(arrl, exit, n);
     return 0;
 }
