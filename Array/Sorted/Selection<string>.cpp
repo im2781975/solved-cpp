dcf8f246ca839923510d8689764b18d7085e669a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-25 10:33:27 +0100

    Update and rename Sort string to Selection<string>.cpp

--- Array/Sorted/Selection<string>.cpp
+++ Array/Sorted/Selection<string>.cpp
@@ -1,28 +1,23 @@
-// C++ implementation to print array of strings in sorted
-// order without copying one string into another
+// print array of strings in sorted order without copying one string into another
 #include <bits/stdc++.h>
- 
 using namespace std;
- 
-// function to print strings in sorted order
+
 void printInSortedOrder(string arr[], int n)
 {
     int index[n];
     int i, j, min;
      
-    // Initially the index of the strings
-    // are assigned to the 'index[]' 
+    // Initially the index of the strings are assigned to the 'index[]' 
     for (i=0; i<n; i++)
         index[i] = i;
      
-    // selection sort technique is applied    
+    // selection sort 
     for (i=0; i<n-1; i++)    
     {
         min = i;
         for (j=i+1; j<n; j++)
         {
-            // with the help of 'index[]'
-            // strings are being compared
+            //with the help of 'index[]' strings are being compared
             if (arr[index[min]].compare(arr[index[j]]) > 0)
                 min = j;
         }
@@ -36,13 +31,9 @@ void printInSortedOrder(string arr[], int n)
             index[i] = temp;
         }
     }
-     
-    // printing strings in sorted order
     for (i=0; i<n; i++)
-        cout << arr[index[i]] << " ";
+        cout <<arr[index[i]]<< " ";
 }
- 
-// Driver program to test above
 int main()
 {
     string arr[] = {"geeks", "quiz", "geeks", "for"};
