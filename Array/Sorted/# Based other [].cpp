2e2c840fc3fb322888013471342dd29b747c63ed
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-25 09:02:20 +0100

    Update and rename Sort an array to # Based other [].cpp

--- Array/Sorted/# Based other [].cpp
+++ Array/Sorted/# Based other [].cpp
@@ -1,25 +1,16 @@
-// A C++ program to sort an array according to the order
-// defined by another array
+//sort an array according to the order defined by another array
 #include <bits/stdc++.h>
 using namespace std;
- 
-// A Binary Search based function to find index of FIRST
+ // A Binary Search based function to find index of FIRST
 // occurrence of x in arr[].  If x is not present, then it
 // returns -1
- 
-// The same can be done using the lower_bound
-// function in C++ STL
 int first(int arr[], int low, int high, int x, int n)
 {
- 
-    // Checking condition
-    if (high >= low) {
- 
-        // FInd the mid element
+    if (high >= low) 
+    {
         int mid = low + (high - low) / 2;
- 
-        // Check if the element is the extreme left
-        // in the left half of the array
+
+        // if the element is the extreme left
         if ((mid == 0 || x > arr[mid - 1]) && arr[mid] == x)
             return mid;
  
@@ -30,25 +21,18 @@ int first(int arr[], int low, int high, int x, int n)
         // Check for element in the left half
         return first(arr, low, (mid - 1), x, n);
     }
- 
-    // ELement not found
+    
     return -1;
 }
- 
-// Sort A1[0..m-1] according to the order defined by
-// A2[0..n-1].
 void sortAccording(int A1[], int A2[], int m, int n)
 {
     // The temp array is used to store a copy of A1[] and
-    // visited[] is used mark the visited elements in
-    // temp[].
+    // visited[] is used mark the visited elements in temp[].
     int temp[m], visited[m];
     for (int i = 0; i < m; i++) {
         temp[i] = A1[i];
         visited[i] = 0;
     }
- 
-    // Sort elements in temp
     sort(temp, temp + m);
  
     // for index of output which is sorted A1[]
@@ -78,18 +62,13 @@ void sortAccording(int A1[], int A2[], int m, int n)
         if (visited[i] == 0)
             A1[ind++] = temp[i];
 }
- 
-// Utility function to print an array
+
 void printArray(int arr[], int n)
 {
- 
-    // Iterate in the array
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
- 
-// Driver Code
 int main()
 {
     int A1[] = { 2, 1, 2, 5, 7, 1, 9, 3, 6, 8, 8 };
@@ -97,7 +76,6 @@ int main()
     int m = sizeof(A1) / sizeof(A1[0]);
     int n = sizeof(A2) / sizeof(A2[0]);
  
-    // Prints the sorted array
     cout << "Sorted array is \n";
     sortAccording(A1, A2, m, n);
     printArray(A1, m);
