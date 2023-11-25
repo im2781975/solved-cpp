6a6ec77258d26136b5b4f211c6bc82f8de9e85f0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-25 10:49:13 +0100

    Update and rename Sort<pair 1> to Pair.cpp

---
+++ Array/Sorted/Pair.cpp
@@ -0,0 +1,33 @@
+// sorting in vector of pair according to 2nd element of pair
+#include<bits/stdc++.h>
+using namespace std;
+//sort by second element
+bool sortbysec(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return (a.second < b.second);
+}
+int main()
+{
+    vector< pair <int, int> > vect;
+
+    int arr[] = {10, 20, 5, 40 };
+    int arr1[] = {30, 60, 20, 50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+ 
+    for (int i=0; i<n; i++)
+        vect.push_back( make_pair(arr[i],arr1[i]) );
+ 
+    cout << "The vector before sort operation is:\n" ;
+    for (int i=0; i<n; i++)
+    {
+        cout << vect[i].first << " "<< vect[i].second << endl;
+    }
+    sort(vect.begin(), vect.end(), sortbysec);
+ 
+    cout << "The vector after sort operation is:\n" ;
+    for (int i=0; i<n; i++)
+    {
+        cout << vect[i].first << " "<< vect[i].second << endl;
+    }
+    return 0;
+}
