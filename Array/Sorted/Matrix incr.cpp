67cd92ca504b46c4abd4f9a05b1ecdee7d89d7e9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-25 10:40:38 +0100

    Update and rename Sort<matrix> to Matrix incr.cpp

--- Array/Sorted/Matrix incr.cpp
+++ Array/Sorted/Matrix incr.cpp
@@ -1,18 +1,14 @@
-// C++ program to sort matrix in all-way 
+//sort matrix in all-way 
 #include<bits/stdc++.h> 
 using namespace std; 
 #define N 3 
-  
-// Sorts a matrix in increasing order 
+
 void sortAllWay(int arr[][N]) 
 { 
-    // Consider matrix elements (in row major 
-    // order) and sort the sequence. 
+    // Consider matrix elements (in row major order) and sort the sequence. 
     int *ptr = (int *)arr; 
     sort(ptr, ptr+N*N); 
 } 
-  
-// driver program 
 int main() 
 { 
     int arr[N][N] = {1, 0, 3, 
@@ -20,14 +16,11 @@ int main()
                      9, 4, 8}; 
     sortAllWay(arr); 
   
-  
-    // print resultant matrix 
     for (int i=0; i<N; i++) 
     { 
         for (int j=0; j<N; j++) 
-            cout << arr[i][j] << " "; 
+           { cout <<arr[i][j]<< " "; }
         cout <<"\n"; 
     } 
-  
     return 0; 
 } 
