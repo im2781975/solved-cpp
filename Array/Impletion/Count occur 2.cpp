eaef71b9019a56c8a07509057cf4e588e2f01535
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-26 21:04:20 +0100

    Update and rename Count appearence<3> to Count occur 2.cpp

--- Array/Impletion/Count occur 2.cpp
+++ Array/Impletion/Count occur 2.cpp
@@ -9,19 +9,32 @@ int singleelement(int arr[], int n)
           //storing frequency 
           mm[arr[i]]++; 
     } 
-        
-      //iterating over map 
-      for(auto x:mm) 
+    for(auto x:mm) 
     { 
           //returning element with frequency 1 
           if(x.second==1) return x.first; 
     } 
+} 
+int singleElement(int arr[], int n) 
+{ 
+    int low = 0, high = n - 2; 
+    int mid; 
+    while (low <= high) { 
+        mid = (low + high) / 2; 
+        if (arr[mid] == arr[mid ^ 1]) 
+            low = mid + 1; 
+        else
+            high = mid - 1; 
+    } 
+    return arr[low]; 
 } 
 int main() 
 { 
     int arr[] = { 2, 3, 5, 4, 5, 3, 4 }; 
     int size = sizeof(arr) / sizeof(arr[0]); 
     sort(arr, arr + size); 
-    cout << singleelement(arr, size); 
+    cout << singleelement(arr, size)<<"\n";
+    cout << singleElement(arr, size); 
+    
     return 0; 
-}
+} 
