99ba3698ba6efa4b9f53de20924ebf1a5a6e99ad
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-26 07:00:16 +0100

    Update and rename Array/Element_sort<single> to Array/Assemble/Merge halves.cpp

--- Array/Assemble/Merge halves.cpp
+++ Array/Assemble/Merge halves.cpp
@@ -1,26 +1,20 @@
-// C++ program to Merge Two Sorted Halves Of 
-// Array Into Single Sorted Array 
+// Merge Two Sorted Halves Of Array Into Single Sorted Array 
 #include <bits/stdc++.h> 
 using namespace std; 
-  
-// Merge two sorted halves of Array into single 
-// sorted array 
 void mergeTwoHalf(int A[], int n) 
 { 
-    int half_i = 0; // starting index of second half 
-  
-    // Temp Array store sorted resultant array 
+    // starting index of second half 
+    int half_i = 0; 
+    //to store resultant array
     int temp[n]; 
   
-    // First Find the point where array is divide 
-    // into two half 
+    // Find the point where array is divide into two half 
     for (int i = 0; i < n - 1; i++) { 
         if (A[i] > A[i + 1]) { 
             half_i = i + 1; 
             break; 
         } 
     } 
-  
     // If Given array is all-ready sorted 
     if (half_i == 0) 
         return; 
@@ -33,27 +27,22 @@ void mergeTwoHalf(int A[], int n)
         else
             temp[k++] = A[j++]; 
     } 
-  
-    // Copy the remaining elements of A[i to half_! ] 
+    // Copy the remaining 
     while (i < half_i) 
         temp[k++] = A[i++]; 
-  
-    // Copy the remaining elements of A[ half_! to n ] 
+    // Copy the remaining 
     while (j < n) 
         temp[k++] = A[j++]; 
   
     for (int i = 0; i < n; i++) 
         A[i] = temp[i]; 
 } 
-  
-// Driver code 
 int main() 
 { 
-    int A[] = { 2, 3, 8, -1, 7, 10 }; 
+    int A[]{ 2, 3, 8, -1, 7, 10 }; 
     int n = sizeof(A) / sizeof(A[0]); 
     mergeTwoHalf(A, n); 
   
-    // Print sorted Array 
     for (int i = 0; i < n; i++) 
         cout << A[i] << " "; 
     return 0; 
