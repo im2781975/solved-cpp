59d6ef85cde05e7a7a50628bdaf7b33cd669baaa
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-26 08:40:29 +0100

    Update and rename Array/Elements_sort<comp> to Array/Assemble/By freq.cpp

--- Array/Assemble/By freq.cpp
+++ Array/Assemble/By freq.cpp
@@ -1,11 +1,7 @@
-
-// CPP program for above approach
 #include <bits/stdc++.h>
 using namespace std;
  
-// Compare function
-bool fcompare(pair<int, pair<int, int> > p,
-              pair<int, pair<int, int> > p1)
+bool fcompare(pair<int, pair<int, int> > p,pair<int, pair<int, int> > p1)
 {
     if (p.second.second != p1.second.second)
         return (p.second.second > p1.second.second);
@@ -20,9 +16,9 @@ void sortByFrequency(int arr[], int n)
             hash[arr[i]].second++;
         else
             hash[arr[i]] = make_pair(i, 1);
-    } // store the count of all the elements in the hashmap
- 
-    // Iterator to Traverse the Hashmap
+    } 
+    // store the count of all the elements in the hashmap
+
     auto it = hash.begin();
  
     // Vector to store the Final Sortted order
@@ -32,21 +28,17 @@ void sortByFrequency(int arr[], int n)
  
     sort(b.begin(), b.end(), fcompare);
  
-    // Printing the Sorted sequence
     for (int i = 0; i < b.size(); i++) {
         int count = b[i].second.second;
         while (count--)
             cout << b[i].first << " ";
     }
 }
- 
-// Driver code
 int main()
 {
     int arr[] = { 2, 5, 2, 6, -1, 9999999, 5, 8, 8, 8 };
     int n = sizeof(arr) / sizeof(arr[0]);
  
-    // Function call
     sortByFrequency(arr, n);
  
     return 0;
