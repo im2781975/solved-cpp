098ab39af4b62d29c5e5bda4a0ad17d0419280a8
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-26 18:45:37 +0100

    Update and rename Meta_binary to Binary meta.cpp

--- Array/Probe/Binary meta.cpp
+++ Array/Probe/Binary meta.cpp
@@ -1,11 +1,8 @@
-// C++ implementation of above approach
- 
 #include <iostream>
 #include <cmath>
 #include <vector>
 using namespace std;
- 
-// Function to show the working of Meta binary search
+
 int bsearch(vector<int> A, int key_to_search)
 {
     int n = (int)A.size();
@@ -17,30 +14,23 @@ int bsearch(vector<int> A, int key_to_search)
  
     int pos = 0;
     for (int i = lg ; i >= 0; i--) {
-        if (A[pos] == key_to_search)
+        if (A[pos] ==key_to_search)
             return pos;
  
-        // Incrementally construct the
-        // index of the target value
+        // Incrementally construct the index of the target value
         int new_pos = pos | (1 << i);
  
-        // find the element in one
-        // direction and update position
+        // find the element in one direction and update position
         if ((new_pos < n) && (A[new_pos] <= key_to_search))
             pos = new_pos;
     }
- 
     // if element found return pos otherwise -1
     return ((A[pos] == key_to_search) ? pos : -1);
 }
- 
-// Driver code
 int main(void)
 {
- 
     vector<int> A = { -2, 10, 100, 250, 32315 };
     cout << bsearch(A, 10) << endl;
  
     return 0;
 }
- 
