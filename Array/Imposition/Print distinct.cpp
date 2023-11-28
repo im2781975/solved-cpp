bf32aeb1c5c1244bbba462a04961f71ffa2f96f0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-28 07:35:13 +0100

    Update and rename Print_distinct<> to Print distinct.cpp

---
+++ Array/Imposition/Print distinct.cpp
@@ -0,0 +1,38 @@
+//print all distinct elements in a given array
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void printDistinct(int arr[], int n)
+{
+    for (int i=0; i<n; i++)
+    {
+        // Check if the picked element is already printed
+        int j;
+        for (j=0; j<i; j++)
+           if (arr[i] == arr[j])
+               break;
+        // If not printed earlier, then print it
+        if (i == j)
+          cout << arr[i] << " ";
+    }
+}
+void print_Distinct(int arr[], int n)
+{
+    sort(arr, arr + n);
+    for (int i=0; i<n; i++)
+    {
+       // Move the index ahead while there are duplicates
+       while (i < n-1 && arr[i] == arr[i+1])
+          i++;
+    // print last occurrence of the current element
+       cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int arr[] = {6, 10, 5, 4, 9, 120, 4, 6, 10};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printDistinct(arr, n);
+    print_Distinct(arr, n);
+    return 0;
+}
