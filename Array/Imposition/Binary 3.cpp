318ce8e3124d86addf654ffbaeb594717d4b3221
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-28 14:29:43 +0100

    Update and rename Search to Binary 3.cpp

--- Array/Imposition/Binary 3.cpp
+++ Array/Imposition/Binary 3.cpp
@@ -1,10 +1,6 @@
-// C++ Program to search an element
-// in a sorted and pivoted array
- 
+// search an element in a sorted and pivoted array
 #include <bits/stdc++.h>
 using namespace std;
- 
-// Standard Binary Search function
 int binarySearch(int arr[], int low, int high, int key)
 {
     if (high < low)
@@ -19,12 +15,9 @@ int binarySearch(int arr[], int low, int high, int key)
  
     return binarySearch(arr, low, (mid - 1), key);
 }
- 
-// Function to get pivot. For array 3, 4, 5, 6, 1, 2
-// it returns 3 (index of 6)
+
 int findPivot(int arr[], int low, int high)
 {
-    // Base cases
     if (high < low)
         return -1;
     if (high == low)
@@ -43,20 +36,15 @@ int findPivot(int arr[], int low, int high)
  
     return findPivot(arr, mid + 1, high);
 }
- 
-// Searches an element key in a pivoted
-// sorted array arr[] of size n
+// Searches an element key in a pivoted sorted array arr[] of size n
 int pivotedBinarySearch(int arr[], int n, int key)
 {
     int pivot = findPivot(arr, 0, n - 1);
  
-    // If we didn't find a pivot,
-    // then array is not rotated at all
+    // If we didn't find a pivot,then array is not rotated at all
     if (pivot == -1)
         return binarySearch(arr, 0, n - 1, key);
- 
-    // If we found a pivot, then first compare with pivot
-    // and then search in two subarrays around pivot
+    // If we found a pivot, then first compare with pivot & then search in two subarrays around pivot
     if (arr[pivot] == key)
         return pivot;
  
@@ -65,18 +53,12 @@ int pivotedBinarySearch(int arr[], int n, int key)
  
     return binarySearch(arr, pivot + 1, n - 1, key);
 }
- 
-// Driver program to check above functions
 int main()
 {
-    // Let us search 3 in below array
     int arr1[] = { 5, 6, 7, 8, 9, 10, 1, 2, 3 };
     int n = sizeof(arr1) / sizeof(arr1[0]);
     int key = 3;
  
-    // Function calling
-    cout << "Index of the element is : "
-         << pivotedBinarySearch(arr1, n, key);
- 
+    cout << "\nIndex of the element is : "<< pivotedBinarySearch(arr1, n, key);
     return 0;
 }
