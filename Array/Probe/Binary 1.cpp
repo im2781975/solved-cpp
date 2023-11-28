b32da8626b8894cc175747fa9a03c1a845b0f86b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-28 15:57:10 +0100

    Update Binary 1.cpp

--- Array/Probe/Binary 1.cpp
+++ Array/Probe/Binary 1.cpp
@@ -13,6 +13,29 @@ int binarySearch(int arr[], int low, int high, int key)
         return binarySearch(arr, (mid + 1), high, key); 
     return binarySearch(arr, low, (mid - 1), key); 
 } 
+int search(int arr[], int l, int h, int key)
+{
+    if (l > h)
+        return -1;
+ 
+    int mid = (l + h) / 2;
+    if (arr[mid] == key)
+        return mid;
+        
+    if (arr[l] <= arr[mid]) {
+    //As this subarray is sorted,we can quickly check if key lies in half or other half 
+        if (key >= arr[l] && key <= arr[mid])
+            return search(arr, l, mid - 1, key);
+    //If key not lies in first half subarray,Divide other half  into two subarrays,
+    //such that we can quickly check if key liesin other half 
+        return search(arr, mid + 1, h, key);
+    }
+    //If arr[l..mid] first subarray is not sorted, then arr[mid>] must be sorted subarray
+    if (key >= arr[mid] && key <= arr[h])
+        return search(arr, mid + 1, h, key);
+ 
+    return search(arr, l, mid - 1, key);
+}
 int main() 
 { 
     int arr[] = { 5, 6, 7, 8, 9, 10 }; 
@@ -21,6 +44,7 @@ int main()
     n = sizeof(arr) / sizeof(arr[0]); 
     key = 10; 
  
-    cout << "Index: " << binarySearch(arr, 0, n - 1, key) ;
+    cout << "\nIndex: " << search(arr, 0, n - 1, key) ;
+        cout << "Index: " << binarySearch(arr, 0, n - 1, key) ;
     return 0; 
 } 
