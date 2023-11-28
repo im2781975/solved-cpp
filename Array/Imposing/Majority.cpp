9a9ca08cd7269c2e7f6c9b2c5a991d218a97f0c1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-28 21:14:57 +0100

    Update and rename Majority<1> to Majority.cpp

--- Array/Imposing/Majority.cpp
+++ Array/Imposing/Majority.cpp
@@ -1,10 +1,8 @@
-// C++ Program for finding out
-// majority element in an array 
+// finding out majority element in an array 
 #include <bits/stdc++.h>
 using namespace std;
- 
-/* Function to find the candidate for Majority */
-int findCandidate(int a[], int size)
+
+int findCandidate(int a[],int size)
 {
     int maj_index = 0, count = 1;
     for (int i = 1; i < size; i++) {
@@ -20,45 +18,54 @@ int findCandidate(int a[], int size)
     return a[maj_index];
 }
  
-/* Function to check if the candidate
-   occurs more than n/2 times */
+//Function to check if the candidate occurs more than n/2 times 
 bool isMajority(int a[], int size, int cand)
 {
     int count = 0;
     for (int i = 0; i < size; i++)
- 
         if (a[i] == cand)
             count++;
  
     if (count > size / 2)
         return 1;
- 
     else
         return 0;
 }
- 
-/* Function to print Majority Element */
 void printMajority(int a[], int size)
 {
-    /* Find the candidate for Majority*/
+    // Find the candidate for Majority
     int cand = findCandidate(a, size);
- 
-    /* Print the candidate if it is Majority*/
+    //Print the candidate if it is Majority
     if (isMajority(a, size, cand))
         cout << " " << cand << " ";
- 
     else
         cout << "No Majority Element";
 }
- 
-/* Driver code */
+void findMajority(int arr[], int size)
+{
+    unordered_map<int, int> m;
+    for(int i = 0; i < size; i++)
+        m[arr[i]]++;
+    int count = 0;
+    for(auto i : m)
+    {
+        if(i.second > size / 2)
+        {
+            count =1;
+            cout << "Majority found :- " << i.first<<endl;
+            break;
+        }
+    }
+    if(count == 0)
+        cout << "No Majority element" << endl;
+}
 int main()
 {
     int a[] = { 1, 3, 3, 1, 2 };
     int size = (sizeof(a)) / sizeof(a[0]);
  
-    // Function calling
     printMajority(a, size);
- 
+    findMajority(a, size); 
+    
     return 0;
 }
