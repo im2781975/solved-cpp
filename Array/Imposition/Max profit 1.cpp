886cbd98691b119eb14c88854187060cc1989a57
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-28 13:04:22 +0100

    Update and rename Max profit<1> to Max profit 1.cpp

--- Array/Imposition/Max profit 1.cpp
+++ Array/Imposition/Max profit 1.cpp
@@ -1,44 +1,31 @@
 #include <bits/stdc++.h>
- 
 using namespace std;
- 
-int f(int idx, int buy, int prices[],
-       int cap, int n)
+int f(int idx, int buy, int prices[], int cap, int n)
 {
     if (cap == 0) {
         return 0;
     }
- 
     if (idx == n) {
         return 0;
     }
- 
- 
- 
     int profit = 0;
     // you can either buy or not buy
     if (buy == 0) {
-      profit
-            = max(-prices[idx]
-                      + f(idx + 1, 1, prices,  cap, n),
-                  f(idx + 1, 0, prices,  cap, n));
+      profit = max(-prices[idx]
+     + f(idx + 1, 1, prices,  cap, n),f(idx + 1, 0, prices,  cap, n));
     }
     // you can either sell or not sell
     else {
-         profit = max(
-            prices[idx]
-                + f(idx + 1, 0, prices, cap - 1, n),
-            f(idx + 1, 1, prices,  cap, n));
+         profit = max( prices[idx]
+        + f(idx + 1, 0, prices, cap - 1, n),f(idx + 1, 1, prices,  cap, n));
     }
- 
     return profit;
 }
- 
 int maxtwobuysell(int prices[], int n)
 {
     return f(0, 0, prices, 2, n);
 }
- 
+
 int main()
 {
  
