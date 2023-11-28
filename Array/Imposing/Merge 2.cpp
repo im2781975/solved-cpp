e1f943752ca580b5c0627b7cc367cbb1da7a762d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-28 21:34:10 +0100

    Update and rename Merge array<> to Merge 2.cpp

--- Array/Imposing/Merge 2.cpp
+++ Array/Imposing/Merge 2.cpp
@@ -1,9 +1,21 @@
-// CPP program for the above approach
 #include <bits/stdc++.h>
 using namespace std;
  
-// Function to merge two arrays
 void merge(int arr1[], int arr2[], int n, int m)
+{
+    int i = 0;
+    // while loop till last element of array 1(sorted) is greater than first element of array 2(sorted)
+    while (arr1[n - 1] > arr2[0]) {
+        if (arr1[i] > arr2[0]) {
+            // swap arr1[i] with first element of arr2 and sorting the updated arr2(arr1 is already sorted)
+            swap(arr1[i], arr2[0]);
+            sort(arr2, arr2 + m);
+        }
+        i++;
+    }
+}
+// Function to merge two arrays
+void Merge(int arr1[], int arr2[], int n, int m)
 {
     int i = 0, j = 0, k = n - 1;
  
@@ -23,8 +35,6 @@ void merge(int arr1[], int arr2[], int n, int m)
     // Sort second array
     sort(arr2, arr2 + m);
 }
- 
-// Driver Code
 int main()
 {
  
@@ -32,8 +42,8 @@ int main()
     int ar2[] = { 2, 3, 8, 13 };
     int m = sizeof(ar1) / sizeof(ar1[0]);
     int n = sizeof(ar2) / sizeof(ar2[0]);
-    merge(ar1, ar2, m, n);
- 
+   // merge(ar1, ar2, m, n);
+    Merge(ar1, ar2, m, n);
     cout << "After Merging \nFirst Array: ";
     for (int i = 0; i < m; i++)
         cout << ar1[i] << " ";
@@ -42,3 +52,4 @@ int main()
         cout << ar2[i] << " ";
     return 0;
 }
+
