157c065c819af743154d8db8ea1377478be5d36f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-28 21:54:30 +0100

    Update and rename Smallest subarray to Smallest subarray.cpp

--- Array/Imposing/Smallest subarray.cpp
+++ Array/Imposing/Smallest subarray.cpp
@@ -32,8 +32,35 @@ int smallestSubWithSum(int arr[], int n, int x)
      }
      return min_len;
 }
+// Returns length of smallest subarray with sum greater than
+// x. If there is no subarray with given sum, then returns
+// n+1
+int smallestSubWithSum(int arr[], int n, int x)
+{
+    // Initialize current sum and minimum length
+    int curr_sum = 0, min_len = n + 1;
  
-/* Driver program to test above function */
+    // Initialize starting and ending indexes
+    int start = 0, end = 0;
+    while (end < n) {
+        // Keep adding array elements while current sum
+        // is smaller than or equal to x
+        while (curr_sum <= x && end < n)
+            curr_sum += arr[end++];
+ 
+        // If current sum becomes greater than x.
+        while (curr_sum > x && start < n) {
+            // Update minimum length if needed
+            if (end - start < min_len)
+                min_len = end - start;
+ 
+            // remove starting elements
+            curr_sum -= arr[start++];
+        }
+    }
+    return min_len;
+}
+
 int main()
 {
     int arr1[] = {1, 4, 45, 6, 10, 19};
