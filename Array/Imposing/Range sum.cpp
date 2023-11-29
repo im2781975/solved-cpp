5e688446c39a6c92b895907cf33fdcc3a9b57c8d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-29 17:26:55 +0100

    Update and rename Range_sum to Range sum.cpp

--- Array/Imposing/Range sum.cpp
+++ Array/Imposing/Range sum.cpp
@@ -1,33 +1,23 @@
-// CPP program to find the sum in a given
-// range in an array using sparse table.
- 
+//find the sum in a given range in an array using sparse table.
 #include <bits/stdc++.h>
 using namespace std;
- 
 // Because 2^17 is larger than 10^5
 const int k = 16;
- 
 // Maximum value of array
 const int N = 1e5;
- 
-// k + 1 because we need to access
-// table[r][k]
+// k + 1 because we need to access table[r][k]
 long long table[N][k + 1];
- 
 // it builds sparse table.
 void buildSparseTable(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
         table[i][0] = arr[i];
- 
     for (int j = 1; j <= k; j++)
         for (int i = 0; i <= n - (1 << j); i++)
             table[i][j] = table[i][j - 1] +
                table[i + (1 << (j - 1))][j - 1];
 }
- 
-// Returns the sum of the elements in the range
-// L and R.
+// Returns the sum of the elements in the range L and R.
 long long query(int L, int R)
 {
     // boundaries of next query, 0-indexed
@@ -36,15 +26,12 @@ long long query(int L, int R)
         if (L + (1 << j) - 1 <= R) {
             answer = answer + table[L][j];
  
-            // instead of having L', we
-            // increment L directly
+            // instead of having L', we increment L directly
             L += 1 << j;
         }
     }
     return answer;
 }
- 
-// Driver program.
 int main()
 {
     int arr[] = { 3, 7, 2, 5, 8, 9 };
