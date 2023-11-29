2c446906d2663e8b0a99a4f442d7a6e1598c7eb3
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-29 17:34:54 +0100

    Update and rename Sort<0> to Sort.cpp

---
+++ Array/Sorted/Sort.cpp
@@ -0,0 +1,65 @@
+//sort a nearly sorted array.
+#include <bits/stdc++.h>
+using namespace std;
+  
+void sortNearlySortedArray(int A[], int size, int k)
+{
+    int i, key, j;
+    for (i = 1; i < size; i++) {
+        key = A[i];
+        j = i - 1;
+  
+        /* Check if the previous element is greater than the current element, and shift elements to the right until the correct position is found, but only if the current element is more
+           than k positions away from its correct position */
+        while (j >= max(0, i - k) && A[j] > key) {
+            A[j + 1] = A[j];
+            j--;
+        }
+        A[j + 1] = key;
+    }
+    for(int i=0; i<size; i++)
+        cout<<A[i]<<" ";
+    cout<<endl;
+}
+// Given an array of size n, where every element is k away from its target position.
+void sortK(int arr[], int n, int k)
+{
+ 
+    // Insert first k+1 items in a priority queue (or min
+    // heap)(A O(k) operation). We assume, k < n. if size of array = k i.e k away from its target position then
+    int size;
+    size = (n == k) ? k : k + 1;
+    priority_queue<int, vector<int>, greater<int> > pq(
+        arr, arr + size);
+ 
+    // i is index for remaining elements in arr[] and index is target index of for current minimum element in Min Heap 'pq'.
+    int index = 0;
+    // here even if size=k then n will be n=k,so i<n works fine
+    for (int i = k + 1; i < n; i++) {
+        arr[index++] = pq.top();
+        pq.pop();
+        pq.push(arr[i]);
+    }
+ 
+    while (pq.empty() == false) {
+        arr[index++] = pq.top();
+        pq.pop();
+    }
+}
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+int main()
+{
+    int A[] = {2, 6, 3, 12, 56, 8};
+    int size = 6;
+    int k = 3;
+    sortNearlySortedArray(A, size, k);   
+    
+    sortK(arr, n, k);
+    printArray(arr, n);
+    return 0;
+}
