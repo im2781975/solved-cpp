a364b69ef1da898c1a414a4180d5ab0de140bbb6
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-29 17:52:34 +0100

    Update and rename Sparse table to Sparse table.cpp

--- Array/Imposing/Sparse table.cpp
+++ Array/Imposing/Sparse table.cpp
@@ -1,30 +1,21 @@
-// C++ program to do range minimum query
-// using sparse table
+//do range minimum query using sparse table
 #include <bits/stdc++.h>
 using namespace std;
 #define MAX 500
  
-// lookup[i][j] is going to store minimum
-// value in arr[i..j]. Ideally lookup table
-// size should not be fixed and should be
-// determined using n Log n. It is kept
-// constant to keep code simple.
+// lookup[i][j] is going to store minimum value in arr[i..j]. Ideally lookup table
+// size should not be fixed and should be determined using n Log n. It is kept constant to keep code simple.
 int lookup[MAX][MAX];
- 
 // Fills lookup array lookup[][] in bottom up manner.
 void buildSparseTable(int arr[], int n)
 {
     // Initialize M for the intervals with length 1
     for (int i = 0; i < n; i++)
         lookup[i][0] = arr[i];
- 
     // Compute values from smaller to bigger intervals
     for (int j = 1; (1 << j) <= n; j++) {
- 
-        // Compute minimum value for all intervals with
-        // size 2^j
+        // Compute minimum value for all intervals with size 2^j
         for (int i = 0; (i + (1 << j) - 1) < n; i++) {
- 
             // For arr[2][10], we compare arr[lookup[0][7]] 
             // and arr[lookup[3][10]]
             if (lookup[i][j - 1] < 
@@ -55,8 +46,41 @@ int query(int L, int R)
     else
         return lookup[R - (1 << j) + 1][j];
 }
+// lookup[i][j] is going to store GCD of
+// arr[i..j]. Ideally lookup table
+// size should not be fixed and should be
+// determined using n Log n. It is kept
+// constant to keep code simple.
+int table[MAX][MAX];
  
-// Driver program
+// it builds sparse table.
+void buildSparseTable(int arr[], int n)
+{
+    // GCD of single element is element itself
+    for (int i = 0; i < n; i++)
+        table[i][0] = arr[i];
+ 
+    // Build sparse table
+    for (int j = 1; j <= log2(n); j++)
+        for (int i = 0; i <= n - (1 << j); i++)
+            table[i][j] = __gcd(table[i][j - 1],
+                    table[i + (1 << (j - 1))][j - 1]);
+}
+ 
+// Returns GCD of arr[L..R]
+int query(int L, int R)
+{
+    // Find highest power of 2 that is smaller
+    // than or equal to count of elements in given
+    // range.For [2, 10], j = 3
+    int j = (int)log2(R - L + 1);
+ 
+    // Compute GCD of last 2^j elements with first
+    // 2^j elements in range.
+    // For [2, 10], we find GCD of arr[lookup[0][3]] and
+    // arr[lookup[3][3]],
+    return __gcd(table[L][j], table[R - (1 << j) + 1][j]);
+}
 int main()
 {
     int a[] = { 7, 2, 3, 0, 5, 10, 3, 12, 18 };
