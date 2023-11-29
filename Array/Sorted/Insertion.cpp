2faa9f853f06487796592b67ac76a7c9fcb61d39
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-29 15:29:44 +0100

    Update and rename Insertion sort to Insertion.cpp

--- Array/Sorted/Insertion.cpp
+++ Array/Sorted/Insertion.cpp
@@ -1,30 +1,22 @@
-/* C++ Function to sort an array using insertion sort*/
- 
+//sort an array using insertion sort
 #include <bits/stdc++.h>
 using namespace std;
- 
 void insertionSort(int A[], int size)
 {
     int i, key, j;
     for (i = 1; i < size; i++) {
         key = A[i];
         j = i - 1;
- 
-        /* Move elements of A[0..i-1], that are
-           greater than key, to one
-           position ahead of their current position.
-           This loop will run at most k times */
+        //Move elements of A[0..i-1], that are greater than key, to one
+           //position ahead of their current position.This loop will run at most k times 
         while (j >= 0 && A[j] > key) {
             A[j + 1] = A[j];
             j = j - 1;
         }
         A[j + 1] = key;
     }
-   
-  for(int i=0; i<size; i++)
+    for(int i=0; i<size; i++)
       cout<<A[i]<<" ";
-   
-  cout<<endl;
 }
  
 int main()
