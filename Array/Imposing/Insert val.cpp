909923e9ef104dcc81e84dc15a378f14b58154a4
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-11-30 15:28:36 +0100

    Update and rename Linear 2.cpp to Insert val.cpp

--- Array/Imposing/Insert val.cpp
+++ Array/Imposing/Insert val.cpp
@@ -9,6 +9,11 @@ int insertSorted(int arr[], int n, int key, int capacity)
         return n;
     arr[n] = key;
     return (n + 1);
+   /*arr[n] = key;
+If the array is not full, it inserts the value of key at the position n in the array.
+
+   return (n + 1);
+   Returns the updated size of the array after the insertion. If the array was full, it returns the original size n without incrementing it.*/
 }
 int main()
 {
@@ -29,3 +34,4 @@ int main()
 
     return 0;
 }
+
