2bf2b32c82775032ebf2b3732c8a213a112e7b87
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-02 17:04:41 +0100

    Update Find peak 1.cpp

--- Array/Imposing/Find peak 1.cpp
+++ Array/Imposing/Find peak 1.cpp
@@ -10,10 +10,12 @@ int findPeak(int arr[], int n)
     while (l <= r) { 
         // finding mid by binary right shifting. 
         mid = (l + r) >> 1; 
+//midpoint is calculated by right-shifting the sum of l and r by 1 (which is equivalent to dividing by 2)
         // first case if mid is the answer 
         if ((mid == 0 || arr[mid - 1] <= arr[mid]) 
             and (mid == n - 1 || arr[mid + 1] <= arr[mid])) 
             break; 
+        //If mid is a peak (greater than or equal to its neighbors or it is at the boundary), the loop breaks.
         
         if (mid > 0 and arr[mid - 1] > arr[mid]) 
             r = mid - 1; 
