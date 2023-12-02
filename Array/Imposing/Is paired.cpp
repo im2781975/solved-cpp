bc438ba8291883dc58e261efb57868682672d4aa
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-02 17:09:02 +0100

    Update Is paired.cpp

--- Array/Imposing/Is paired.cpp
+++ Array/Imposing/Is paired.cpp
@@ -1,9 +1,5 @@
-/ C++ Program Illustrating Naive Approach to
 // Find if There is a Pair in A[0..N-1] with Given Sum
- 
-// Importing all libraries
 #include <bits/stdc++.h>
- 
 using namespace std;
  
 bool isPairSum(int A[], int N, int X)
@@ -13,29 +9,23 @@ bool isPairSum(int A[], int N, int X)
             // as equal i and j means same element
             if (i == j)
                 continue;
- 
             // pair exists
             if (A[i] + A[j] == X)
                 return true;
- 
             // as the array is sorted
             if (A[i] + A[j] > X)
                 break;
         }
     }
- 
-    // No pair found with given sum.
+    //doesn't find any pair..
     return false;
 }
- 
-// Driver code
 int main()
 {
     int arr[] = { 2, 3, 5, 8, 9, 10, 11 };
     int val = 17;
     int arrSize = *(&arr + 1) - arr;
-    sort(arr, arr + arrSize); // Sort the array
-    // Function call
+    sort(arr, arr + arrSize);
     cout << isPairSum(arr, arrSize, val);
  
     return 0;
