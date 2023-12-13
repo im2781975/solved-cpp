e98d601504d73d7082d4159acf7876cdd925e571
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-13 09:54:23 +0100

    Update Unique 1.cpp

--- STL/Unique 1.cpp
+++ STL/Unique 1.cpp
@@ -20,10 +20,32 @@ void print(vector<int>vect)
         cout<<d[i]<<" ";
     }
 }
+void printDuplicates(int arr[], int n)
+{
+    unordered_set<int> intSet;
+    unordered_set<int> duplicate;
+ 
+    for (int i = 0; i < n; i++) {
+
+        if (intSet.find(arr[i]) == intSet.end())
+            intSet.insert(arr[i]);
+ 
+        else
+        duplicate.insert(arr[i]);
+    }
+ 
+    cout <<"Duplicate item are (Set): ";
+    unordered_set<int>::iterator itr;
+    for (itr = duplicate.begin(); itr != duplicate.end();
+         itr++)
+        cout << *itr << " ";
+}
 int main()
 {
     int arr[]{1,2,3,2,5,7};
     int n=sizeof(arr)/ sizeof(arr[0]);
     vector<int>vect(arr,arr+n);
+    printDuplicates(arr,n);
+    cout <<"\nDuplicate item are (Vector): ";
     print(vect);
 }
