75239a182f1193beef569c0fb102708b6799b239
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-13 16:53:23 +0100

    Update and rename Merge() to Merge().cpp

---
+++ Sorting/Merge().cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(vector<int> st)
+{
+    for(int i = 0; i < st.size(); i++)
+    {
+        cout << st[i] << " ";
+    }
+}
+
+int main()
+{
+    vector<int> vect {2, 3, 6};
+    vector<int> vec {5, 7, 9};
+    vector<int> cev(10);
+    vector<int> v, root(vect.size() + vec.size());
+
+    cout << "\nvect is: ";
+    print(vect);
+    cout << "\nvec is: ";
+    print(vec);
+
+    sort(vect.begin(), vect.end());
+    sort(vec.begin(), vec.end());
+
+    merge(vect.begin(), vect.end(), vec.begin(), vec.end(), cev.begin());
+    cev.resize(distance(cev.begin(), unique(cev.begin(), cev.end())));
+    cout << "\nAfter sorting cev is: ";
+    print(cev);
+
+    includes(v.begin(), v.end(), cev.begin(), cev.end()) ?
+        cout << "\ncev is included in v" :
+        cout << "\ncev isn't included";
+
+    auto it = copy(vect.begin(), vect.end(), root.begin());
+    copy(vec.begin(), vec.end(), it);
+    cout << "\nMerged and copied vector is: ";
+    print(root);
+
+    return 0;
+}
+
