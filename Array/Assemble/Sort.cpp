33f814f3d89740b951d5cd4c402e68e809ad0b7a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-20 19:31:47 +0100

    Update Sort.cpp

--- Array/Assemble/Sort.cpp
+++ Array/Assemble/Sort.cpp
@@ -1,19 +1,16 @@
-/ C++ program for the above approach
- 
 #include <bits/stdc++.h>
 using namespace std;
- 
 int sort(vector<int>& array, int l, int h, int k)
 {
     int mid
-        = l + (h - l) / 2; // Choose middle element as pivot
-    int i = max(l, mid - k), j = i,
-        end = min(mid + k, h); // Set appropriate range
+        = l + (h - l) / 2; 
+    int i = max(l, mid - k), j = i,end = min(mid + k, h);
+        //i starts at the max of l and (mid - k) as like max(0, 6 - 3) = 3.end is set to the minimum of (mid + k) and h . as like min(6 + 3, 12) = 9.
     swap(array[mid],
-         array[end]); // Swap middle and last element to
-                      // avoid extra complications
+         array[end]);
     while (j < end) {
         if (array[j] < array[end]) {
+        //If array[j] (element at index j) is less than array[end] (pivot element at index end), then swap array[i] and array[j], and increment i.After this loop, elements less than the pivot are on the left side of array[i], and elements greater are on the right side.
             swap(array[i++], array[j]);
         }
         j = j + 1;
@@ -30,8 +27,6 @@ void ksorter(vector<int>& array, int l, int h, int k)
         ksorter(array, q + 1, h, k);
     }
 }
- 
-// Driver code
 int main()
 {
     vector<int> array(
@@ -41,11 +36,10 @@ int main()
     for (int& num : array)
         cout << num << ' ';
     cout << endl;
-   
-      // Function call
     ksorter(array, 0, array.size() - 1, k);
     cout << "Array after K sort\n";
     for (int& num : array)
         cout << num << ' ';
     return 0;
 }
+
