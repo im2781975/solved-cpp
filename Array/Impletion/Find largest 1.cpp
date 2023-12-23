3d439e4b7e9fdc18a294e6ca3fd359fc21b0279d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-23 22:01:45 +0100

    Update Find largest 1.cpp

--- Array/Impletion/Find largest 1.cpp
+++ Array/Impletion/Find largest 1.cpp
@@ -1,24 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print2largest(int arr[], int arr_size) {
-    int i, first, second;
-    if (arr_size < 2) {
+void print2largest(int arr[], int n) {
+    if (n < 2) {
         cout << "Invalid Input";
         return;
     }
-    sort(arr, arr + arr_size);
+    sort(arr, arr + n);
     // Start from the second last element as the largest element is at the last
-    for (i = arr_size - 2; i >= 0; i--) {
-        // If the element is not equal to the largest element
-        if (arr[i] != arr[arr_size - 1]) {
+    for (int i = n - 2; i >= 0; i--) {
+        
+        if (arr[i] != arr[n - 1]) {
             cout << "\nThe second largest element is " << arr[i];
             return;
         }
     }
-    cout << "\nThere is no second largest element\n";
+    cout << "\nThere is no second largest element ";
 }
-
 void print2ndlargest(vector<int> vec) {
     set<int>s(vec.begin(), vec.end());
     vec.clear();
@@ -31,7 +29,6 @@ void print2ndlargest(vector<int> vec) {
     cout << "\nThe Second Largest Element in Vector is: ";
     cout<<vec[vec.size()-1];
 }
-
 int main() {
     int arr[] = {12, 35, 1, 10, 34, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
