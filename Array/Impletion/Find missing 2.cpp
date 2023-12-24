2f1823081a772807aae09dbf591167847cd9569d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-24 10:38:09 +0100

    Update Find missing 2.cpp

--- Array/Impletion/Find missing 2.cpp
+++ Array/Impletion/Find missing 2.cpp
@@ -8,8 +8,7 @@ void printmissing(int arr[], int n, int low,int high)
     unordered_set<int> s;
     for (int i = 0; i < n; i++)
         s.insert(arr[i]);
- 
-    // Traverse through the range an print all missing elements
+
     for (int x = low; x <= high; x++)
         if (s.find(x) == s.end())
             cout << x << " ";
@@ -19,17 +18,17 @@ void printMissing(int arr[], int n,
     int low, int high)
 {
     // Create boolean array of size high-low+1, each index i representing whether (i+low)th element found or not.
-    bool points_of_range[high - low + 1] = { false };
+    bool range[high - low + 1] = { false };
  
     for (int i = 0; i < n; i++) {
         // if ith element of arr is in range low to high then mark corresponding index as true in array
         if (low <= arr[i] && arr[i] <= high)
-            points_of_range[arr[i] - low] = true;
+            range[arr[i] - low] = true;
     }
  
     // Traverse through the range & print all elements  whose value is false
     for (int x = 0; x <= high - low; x++) {
-        if (points_of_range[x] == false)
+        if (range[x] == false)
             cout << low + x << " ";
     }
 }
