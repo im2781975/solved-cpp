d92331d1b235934acab879c3afd044b24ee8842e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-31 05:50:01 +0100

    Update and rename Get<avg> to Average.cpp

--- Vector/Average.cpp
+++ Vector/Average.cpp
@@ -1,29 +1,21 @@
-// average to two numbers
 #include<bits/stdc++.h>
 using namespace std;
-
 float getAverage(float* arr, int size)
 {
     int sum = 0;
-
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++){
         sum += arr[i];
     }
     return sum / size;
 }
-
 int main()
 {
     float arr[5]{ 10, 20, 30, 40, 50 };
-
     int n = sizeof(arr) / sizeof(arr[0]);
- 
-    // printing array elements
     printf("Array Elements: ");
     for (int i = 0; i < n; i++) {
         printf("%.0f ", arr[i]);
     }
- 
     cout<< "\nAverage is: "<< getAverage(arr,n) ;
     return 0;
 }
