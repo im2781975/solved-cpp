766a89789bb8e31cafb2e557a267ff7093dd0f64
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-31 17:08:21 +0100

    Update and rename Pair to Pair.cpp

--- Vector/Pair.cpp
+++ Vector/Pair.cpp
@@ -1,11 +1,8 @@
-// vector of pairs
 #include<bits/stdc++.h>
 using namespace std;
- 
 int main()
 {
     vector< pair <int,int> > vect;
- 
     int arr[] = {10, 20, 5, 40 };
     int arr1[] = {30, 60, 20, 50};
     int n = sizeof(arr)/sizeof(arr[0]);
@@ -14,9 +11,7 @@ int main()
         vect.push_back( make_pair(arr[i],arr1[i]) );
  
     for (int i=0; i<n; i++)
-    {
-        cout << vect[i].first << " "<< vect[i].second << endl;
-    }
+        cout << vect[i].first << " "<< vect[i].second <<"\n";
  
     return 0;
 }
