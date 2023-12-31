0368c1b0bb2dd23f322da00fbbd4eaaed5504c72
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-31 16:48:05 +0100

    Update and rename Merge() vector to Merge vector.cpp

--- Vector/Merge vector.cpp
+++ Vector/Merge vector.cpp
@@ -1,42 +1,39 @@
-/*->5
-2 3 4 5 6
-5
-5 6 7 8 9
-<-2 3 4 5 6 7 8 9 */
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
+
+int main() {
     int n, x, m, flag;
     cin >> n;
-    vector<int> a;
-    vector<int> b;
-    vector<int> k;
-    for(int i=0; i<n; i++){
+    vector<int> a, b, k;
+
+    for (int i = 0; i < n; i++) {
         cin >> x;
         a.push_back(x);
         k.push_back(x);
     }
+
     cin >> m;
-    for(int i=0; i<m; i++){
+    for (int i = 0; i < m; i++) {
         cin >> x;
         b.push_back(x);
     }
 
-    for(int i=0; i<m; i++){
+    for (int i = 0; i < m; i++) {
         flag = 0;
-        for(int j=0; j<n; j++){
-            if(b[i] == a[j]){
+        for (int j = 0; j < n; j++) {
+            if (b[i] == a[j]) {
                 flag++;
             }
         }
-        if(flag==0){
+        if (flag == 0) {
             k.push_back(b[i]);
         }
     }
-    for(int i=0; i<k.size(); i++){
+
+    for (int i = 0; i < k.size(); i++) {
         cout << k[i] << " ";
     }
 
     return 0;
 }
+
