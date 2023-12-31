b57913dff3a5dba3626239613b4e59a90516e0e6
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-31 21:38:58 +0100

    Update and rename Find<fib 0> to Fib.cpp

--- Intro/Fib.cpp
+++ Intro/Fib.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 const int N=101;
 int dp[N];
-
 int main()
 {
     int n;
-    cin>>n;
-    dp[1]=1;
-    dp[2]=1;
+    cin>> n;
+    dp[1]= 1;
+    dp[2]= 1;
+    
     for(int i=3; i<=n; i++)
-    {
-        dp[i]=dp[i-1]+dp[i-2];
-    }
+        dp[i]= dp[i-1]+ dp[i-2];
+        
     cout<<dp[n];
 }
