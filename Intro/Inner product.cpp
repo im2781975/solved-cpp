9cb421ce8eee7f47de3b346015a7027c6b64ad90
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-01 13:10:15 +0100

    Update and rename Func<inner_product> to Inner product.cpp

---
+++ Intro/Inner product.cpp
@@ -0,0 +1,26 @@
+#include <functional>
+#include <iostream>
+#include <numeric>
+using namespace std;
+ 
+int fun(int x, int y) 
+{ return x - y; }
+ 
+int fun1(int x, int y) 
+{ return x + y; }
+int main()
+{
+    int var = 200;
+    int a[] = { 10, 15, 20 };
+    int b[] = { 1, 3, 5 };
+ 
+    cout << "\nResult using inner_product ";
+    cout << inner_product(a, a + 3, b, var);
+
+    cout << "\nResult using inner_product with pre-defined function: ";
+    cout << inner_product(a, a + 3, b, var, minus<int>(),divides<int>());
+ 
+    cout << "\nResult using inner_product with user-defined function: ";
+    cout << inner_product(a, a + 3, b, var, fun, fun1);
+    return 0;
+}
