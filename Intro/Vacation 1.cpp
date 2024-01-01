6c273fa0a076a9c03d3b6f68ba40a18805a587fb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-01 17:45:01 +0100

    Update and rename Vacation<iterative> to Vacation 1.cpp

--- Intro/Vacation 1.cpp
+++ Intro/Vacation 1.cpp
@@ -1,11 +1,8 @@
 //count max happiness in  vacation days work which is 3.each two days work will not be same.
 #include <bits/stdc++.h>
 using namespace std;
-
-const int N = 1005; // Adjust the size as needed
-
+const int N = 1005;
 int happiness[N][4], dp[N][4];
-
 int main() {
     int n;
     cin >> n;
@@ -17,16 +14,16 @@ int main() {
     for(int task=1;task<=3;task++) {
         dp[1][task]=happiness[1][task];
     }
-    for(int day=2;day<=n; day++) { // Corrected starting from day 2
-        for(int cur_task=1; cur_task<=3; cur_task++) { // Corrected variable name "cur_task"
+    for(int day=2;day<=n; day++) {
+        for(int cur_task=1; cur_task<=3; cur_task++) {
             int max_profit = 0;
             for (int last = 1; last <= 3; last++) {
-                if (last != cur_task) { // Corrected variable name "cur_task"
+                if (last != cur_task) { 
                     int cur_profit=dp[day-1][last]+happiness[day][cur_task];
                     max_profit=max(max_profit,cur_profit);
                 }
             }
-            dp[day][cur_task] = max_profit; // Updated the dp table with the max_profit
+            dp[day][cur_task] = max_profit;
         }
     }
     int ans=max({dp[n][1],dp[n][2],dp[n][3]);
