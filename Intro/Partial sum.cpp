8aaaab24398950d6500e50a1b33979a5163c3d80
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-01 13:22:12 +0100

    Update and rename Func<partial sum> to Partial sum.cpp

--- Intro/Partial sum.cpp
+++ Intro/Partial sum.cpp
@@ -1,32 +1,24 @@
-//working of partial_sum()
 #include <iostream>
 #include <numeric>
 using namespace std;
 
 int myfun(int x, int y)
 {
-    // the sum of element is twice of its adjacent element
     return x + 2 * y;
 }
- 
 int main()
 {
     int a[] = { 1, 2, 3, 4, 5 };
     int b[5];
 
     partial_sum(a, a + 5, b);
- 
     cout << "Partial Sum - Using Default function: ";
     for (int i = 0; i < 5; i++)
         cout << b[i] << ' ';
-    cout << '\n';
     
     partial_sum(a, a + 5, b, myfun);
- 
-    cout << "Partial sum - Using user defined function: ";
+    cout << "\nPartial sum - Using user defined function: ";
     for (int i = 0; i < 5; i++)
         cout << b[i] << ' ';
-    cout << '\n';
- 
     return 0;
 }
