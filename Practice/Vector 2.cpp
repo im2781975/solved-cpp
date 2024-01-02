9c6fd3e77e302e14cf61cd965cb03aad88f770b4
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-02 05:06:17 +0100

    Update Vector 2.cpp

--- Practice/Vector 2.cpp
+++ Practice/Vector 2.cpp
@@ -1,27 +1,48 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-
-int n = 5;
 
 int main()
 {
+    int n = 5;
+
     vector<int> v(n * n);
-    
-    for(int i = 0; i < n; i++)
-        v.push_back(i);
-    
-    cout<<"\Elements of vector are: ";
-    for(int i = 0; i < n; i++)
-        cout << v[i] << " ";
-        
-    for(int i = 0; i < n*n; i++)
+
+    for (int i = 0; i < n * n; i++)
         v[i] = i;
 
-    int sum=0;
-    cout<<"\nSum of the vector are: ";
-    for(int i=0 ; i<n*n ; i++)
-        sum+= v[i];
+    cout << "Elements of vector are: ";
+    for (int i = 0; i < n * n; i++)
+        cout << v[i] << " ";
+
+    int sum = 0;
+    cout << "\nSum of the vector elements is: ";
+    for (int i = 0; i < n * n; i++)
+        sum += v[i];
+
+    cout << sum;
+
+    vector<int> cev[n + 1];
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+            cev[i].push_back(j);
+    }
+
+    cout << "\nElements of the 2D vector are: ";
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << cev[i][j] << " ";
+        cout << "\n";
+    }
     
-    cout<<sum;
+    vector <int> vect[1];
+    vect[0].push_back(9);
+    vect[0].push_back(2);
+    
+    vector <int> t= vect[0];
+    cout<< t[0];
+    return 0;
 }
 
