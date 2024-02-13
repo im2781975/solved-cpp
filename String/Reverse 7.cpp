682894cbef7a30666c8caea24cc4fb5661f8b75c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-13 17:17:58 +0100

    Create Reverse 7.cpp

---
+++ String/Reverse 7.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printWords(string str)
+{
+    // word variable to store word
+    string word;
+    // making a string stream
+    stringstream iss(str);
+    // Read and print each word.
+    while (iss >> word) {
+        reverse(word.begin(), word.end());
+        cout << word << " ";
+    }
+}
+int main()
+{
+    string s = "GeeksforGeeks is good to learn";
+    printWords(s);
+    return 0;
+}
