d65fb88e4c7947645e8b4bd0365e768a36daad64
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-05-29 11:06:19 +0200

    Update Based value 1.cpp

--- Array/Assemble/Based value 1.cpp
+++ Array/Assemble/Based value 1.cpp
@@ -20,6 +20,26 @@ void sort012(int arr[], int n)
             i++;
     }
 }
+void sort012(int *arr, int n)
+{
+    int l = 0;
+    int r = n -1;
+    int mid = 0;
+    while(mid <= r){
+        switch(arr[mid])
+        {
+            case 0:
+                swap(arr[l++], arr[mid++]);
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(arr[mid], arr[r--]);
+                break;
+        }
+    }
+}
 void print(int *arr, int n)
 {
     for(int i = 0; i < n; i++)
