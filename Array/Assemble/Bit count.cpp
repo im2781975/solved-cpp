28f649ca4b322cb39fd8af8d9b936ea63d8834ff
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-05-29 21:02:08 +0200

    Update Bit count.cpp

--- Array/Assemble/Bit count.cpp
+++ Array/Assemble/Bit count.cpp
@@ -61,6 +61,35 @@ void sortBySetBitCount(int arr[], int n)
 {
     stable_sort(arr, arr + n, cmp);
 }
+// Function to simultaneously sort both arrays using insertion sort
+void insertionSort(int arr[], int aux[], int n)
+{
+    for (int i = 1; i < n; i++) {
+        
+        int key1 = aux[i];
+        int key2 = arr[i];
+        int j = i - 1;
+ 
+        // Move elements of arr[] & aux[],such that elements of aux[] are greater than key1, to one position ahead of their current position
+        while (j >= 0 && aux[j] < key1) {
+            aux[j + 1] = aux[j];
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        aux[j + 1] = key1;
+        arr[j + 1] = key2;
+    }
+}
+
+void sortBySetBitCount(int arr[], int n)
+{
+    int aux[n];
+    for (int i = 0; i < n; i++)
+        aux[i] = countBits(arr[i]);
+ 
+    // Sort arr[] according to values in aux[]
+    insertionSort(arr, aux, n);
+}
 void printArr(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
