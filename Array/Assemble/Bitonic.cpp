35bf5725244f833bd6f51767c19ba8b8e836a2c0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-05-30 11:24:06 +0200

    Update Bitonic.cpp

--- Array/Assemble/Bitonic.cpp
+++ Array/Assemble/Bitonic.cpp
@@ -5,28 +5,60 @@ void bitonicGenerator(int arr[], int n)
 { 
     for (int i = 0; i < n; i++) { 
         if (i % 2==0) 
-            arr[i]=-1*arr[i]; 
+            arr[i]=(-1)*arr[i]; 
     } 
     sort(arr,arr+n); 
     int mid=(n-1)/2; 
+    for (int i = 0; i <= mid; i++)
+        arr[i]=(-1)*arr[i]; 
       
-    // Reverting the changed sign 
-    for (int i = 0; i <= mid; i++) { 
-            arr[i]=-1*arr[i]; 
-        } 
-      
-    // Reverse first half of array 
     reverse(arr,arr+mid+1); 
-    
-    // Reverse second half of array
-    reverse(arr+mid+1,arr+n); 
+    reverse(arr +mid+ 1,arr+n); 
 } 
+void bitonicGenerator(int arr[], int n) 
+{ 
+    int i = 1; 
+    int j = n - 1; 
+  
+    if (j % 2 != 0) 
+        j--; 
+    while (i < j)
+    { 
+        swap(arr[i], arr[j]); 
+        i += 2; j -= 2; 
+    } 
+    sort(arr, arr + (n + 1) / 2);
+    sort(arr + (n + 1) / 2, arr + n, greater<int>()); 
+} 
+void bitonicGenerator(int arr[], int n) 
+{ 
+    vector<int> evenArr; 
+    vector<int> oddArr; 
+    for (int i = 0; i < n; i++)
+    { 
+        if (!(i % 2)) 
+            evenArr.push_back(arr[i]); 
+        else
+            oddArr.push_back(arr[i]); 
+    } 
+    sort(evenArr.begin(), evenArr.end()); 
+    sort(oddArr.begin(), oddArr.end(), greater<int>()); 
+  
+    int i = 0; 
+    for (int j = 0; j < evenArr.size(); j++) 
+        arr[i++] = evenArr[j]; 
+    for (int j = 0; j < oddArr.size(); j++) 
+        arr[i++] = oddArr[j]; 
+} 
+void print(int *arr, int n)
+{
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
 int main() 
 { 
     int arr[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 }; 
     int n = sizeof(arr) / sizeof(arr[0]); 
     bitonicGenerator(arr, n); 
-    for (int i = 0; i < n; i++) 
-        cout << arr[i] << " "; 
-    return 0; 
-} 
+    print(arr, n);
+}
