179eee8aa8063cf641d25ac7d040be8b8a8198c3
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-01 17:52:16 +0200

    Update Count sort.cpp

--- Array/Assemble/Count sort.cpp
+++ Array/Assemble/Count sort.cpp
@@ -1,14 +1,11 @@
-//function to do counting sort of arr[] according to the digit represented by exp.
+//counting sort of arr[] according to the digit represented by exp
 #include<iostream>
 using namespace std;
 int countSort(int arr[], int n, int exp)
 {
- 
-    int output[n]; 
-    int i, count[n] ;
+    int output[n], i, count[n] ;
     for (int i=0; i < n; i++)
        count[i] = 0;
- 
     // Store count of occurrences in count[]
     for (i = 0; i < n; i++)
         count[ (arr[i]/exp)%n ]++;
@@ -16,7 +13,6 @@ int countSort(int arr[], int n, int exp)
     // Change count[i] so that count[i] now contains actual position of this digit in output[]
     for (i = 1; i < n; i++)
         count[i] += count[i - 1];
- 
     // Build the output array
     for (i = n - 1; i >= 0; i--)
     {
@@ -31,26 +27,21 @@ int countSort(int arr[], int n, int exp)
 }
 void sort(int arr[], int n)
 {
-    // counting sort for first digit in base n.
+    // counting sort for first digit in base n & second digit in base n
     countSort(arr, n, 1);
- 
-    //counting sort for second digit in base n. 
     countSort(arr, n, n);
 }
-
 void printArr(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
 }
-
 int main()
 {
     int arr[] = {40, 12, 45, 32, 33, 1, 22};
     int n = sizeof(arr)/sizeof(arr[0]);
     cout << "Given array is\n";
     printArr(arr, n);
- 
     sort(arr, n);
  
     cout << "\nSorted array is\n";
