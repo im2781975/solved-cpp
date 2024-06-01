a7423ed9ba097d2c46dfefbdd86b0b01db4ae3a2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-01 06:42:49 +0200

    Update Unsorted arr.cpp

--- Array/Assemble/Unsorted arr.cpp
+++ Array/Assemble/Unsorted arr.cpp
@@ -1,58 +1,56 @@
 // find the Minimum length Unsorted Subarray, sorting which makes the complete array sorted
 #include<bits/stdc++.h>
 using namespace std;
- 
 void printUnsorted(int arr[],int n)
 {
-    int s = 0, e = n-1, i, max,min; 
-    for (s = 0; s < n-1; s++)
+    int l = 0, r = n-1, i, max,min;
+    for (l = 0; l < n-1; l++)
     {
-        if (arr[s] > arr[s+1])
-        break;
+        if (arr[l] > arr[l+1])
+            break;
     }
-    if (s == n-1)
+    if (l == n-1)
     {
-    cout << "The complete array is sorted";
-    return;
+        cout << "The complete array is sorted";
+        return;
     }
-    for(e = n - 1; e > 0; e--)
+    for(r = n - 1; r > 0; r--)
     {
-        if(arr[e] < arr[e-1])
-        break;
+        if(arr[r] < arr[r-1])
+            break;
     }
-    max = arr[s]; min = arr[s];
-    for(i = s + 1; i <= e; i++)
+    max = arr[l]; min = arr[l];
+    for(i = l + 1; i <= r; i++)
     {
         if(arr[i] > max)
-        max = arr[i];
+            max = arr[i];
         if(arr[i] < min)
-        min = arr[i];
+            min = arr[i];
     }
-    for( i = 0; i < s; i++)
+    for( i = 0; i < l; i++)
     {
         if(arr[i] > min)
         { 
-            s = i;
+            l = i;
             break;
         }     
     } 
-    for( i = n -1; i >= e+1; i--)
+    for( i = n -1; i >= r+1; i--)
     {
         if(arr[i] < max)
         {
-            e = i;
+            r = i;
             break;
         } 
-    } 
-    cout << "The unsorted subarray which makes the given array
-     << "sorted lies between the indices " << s << " and " << e;
+    }
+    cout << "The unsorted subarray which makes the given array sorted lies between the indices " << l << " and " << r;
     return;
 }
 int main()
 {
     int arr[] = {10, 12, 20, 30, 25,40, 32, 31, 35, 50, 60};
-    int arr_size = sizeof(arr)/sizeof(arr[0]);
-    printUnsorted(arr, arr_size);
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printUnsorted(arr, n);
     getchar();
     return 0;
 }
