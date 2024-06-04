65e22bedfdd7cf3e00ccb74ac3c784aad1e28fce
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-04 04:42:16 +0200

    Update Find Max diff.cpp

--- Array/Execution/Find Max diff.cpp
+++ Array/Execution/Find Max diff.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int maxdiff(int arr[], int n)
 {
     unordered_map<int, int> freq;
- 
     // Finding the frequency of each element.
     for (int i = 0; i < n; i++)
         freq[arr[i]]++;
@@ -15,7 +14,8 @@ int maxdiff(int arr[], int n)
     {
         for (int j=0; j<n; j++)
         {
-            //  if the frequency of the element arr[i] is greater than the frequency of the element arr[j], and at the same time, if the value of arr[i] is greater than the value of arr[j]. If both conditions are true, it means that arr[i] has a higher frequency and a greater value compared to arr[j]. In this case, the difference in frequencies (freq[arr[i]] - freq[arr[j]]) is calculated, and if it's greater than the current ans, it updates ans.
+            //  if the frequency & value of the element arr[i] is greater than the frequency & value of the element arr[j] at the same time, it means that arr[i] has a higher frequency and a greater value compared to arr[j].
+            //In this case, the difference in frequencies (freq[arr[i]] - freq[arr[j]]) is calculated, and if it's greater than the current ans, it updates ans.
             if (freq[arr[i]] > freq[arr[j]] && arr[i] > arr[j] )
                 
                 ans = max(ans, freq[arr[i]]-freq[arr[j]]);
@@ -26,6 +26,34 @@ int maxdiff(int arr[], int n)
     }
     return ans;
 }
+int maxdiff(int arr[], int n) 
+{ 
+    unordered_map<int, int> freq; 
+    int dist[n]; 
+    // Finding the frequency of each element. 
+    int j = 0; 
+    for (int i = 0; i < n; i++) 
+    { 
+        if (freq.find(arr[i]) == freq.end()) 
+            dist[j++] = arr[i]; 
+ 
+        freq[arr[i]]++; 
+    } 
+    // Sorting the distinct element
+    sort(dist, dist + j); 
+    int min_freq = n+1; 
+ 
+    //Iterate through all sorted distinct elements For each distinct element, 
+    //maintaining the element with minimum frequency than that element and also finding the maximum  frequency difference 
+    int ans = 0; 
+    for (int i=0; i<j; i++) 
+    { 
+        int cur_freq = freq[dist[i]]; 
+        ans = max(ans, cur_freq - min_freq); 
+        min_freq = min(min_freq, cur_freq); 
+    } 
+    return ans; 
+} 
 int main()
 {
     int arr[] { 3, 1, 3, 2, 3, 2 };
