f898b8edb26ba48c1924a324c954aeabdc1ce0de
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-04 11:55:14 +0200

    Update Count subset.cpp

--- Array/Execution/Count subset.cpp
+++ Array/Execution/Count subset.cpp
@@ -1,12 +1,31 @@
 // find number of subset containing consecutive numbers 
 #include <bits/stdc++.h> 
 using namespace std; 
-  
-// Returns count of subsets with consecutive numbers 
+//hashing approach
+int countSubsets(int arr[], int n)
+{ 
+    unordered_set<int> s; 
+    int count = 0; 
+    
+    for(int i = 0; i < n; i++) { 
+        s.insert(arr[i]); 
+    } 
+    // iterate through the array again and for each element, check if it is the starting element of a subset 
+    for(int i = 0; i < n; i++) { 
+        if(s.find(arr[i]-1) == s.end()) { 
+            int j = arr[i]; 
+            // count the number of consecutive elements and add them to the set 
+            while(s.find(j) != s.end()) { 
+                j++; 
+            } 
+            count++; 
+        } 
+    } 
+    return count; 
+} 
 int numofsubset(int arr[], int n) 
 { 
     sort(arr, arr + n); 
-  
     int count = 1; 
     for (int i = 0; i < n - 1; i++) { 
         // Check if there is beginning of another 
@@ -20,13 +39,7 @@ int main()
 { 
     int arr[] = { 100, 56, 5, 6, 102, 58, 101, 57, 7, 103, 59 }; 
     int n = sizeof(arr) / sizeof(arr[0]); 
-    sort(arr,arr+n);
-    
-    for(int i=0; i<n; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    cout<<"\n";
     cout << numofsubset(arr, n) ;
+    cout << countSubsets(arr, n) ;
     return 0; 
-} 
+}
