5b776efbfbeff59c575f5855325a629ce7b48e5a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-05 17:51:05 +0200

    Update Sparse table.cpp

--- Array/Imposing/Sparse table.cpp
+++ Array/Imposing/Sparse table.cpp
@@ -1,6 +1,38 @@
-//do range minimum query using sparse table
+//find the sum in a given range in an array using sparse table.
 #include <bits/stdc++.h>
 using namespace std;
+// Because 2^17 is larger than 10^5
+const int k = 16;
+// Maximum value of array
+const int N = 1e5;
+// k + 1 because we need to access table[r][k]
+long long table[N][k + 1];
+// it builds sparse table.
+void buildSparseTable(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        table[i][0] = arr[i];
+    for (int j = 1; j <= k; j++)
+        for (int i = 0; i <= n - (1 << j); i++)
+            table[i][j] = table[i][j - 1] +
+               table[i + (1 << (j - 1))][j - 1];
+}
+// Returns the sum of the elements in the range L and R.
+long long query(int L, int R)
+{
+    // boundaries of next query, 0-indexed
+    long long answer = 0;
+    for (int j = k; j >= 0; j--) {
+        if (L + (1 << j) - 1 <= R) {
+            answer = answer + table[L][j];
+ 
+            // instead of having L', we increment L directly
+            L += 1 << j;
+        }
+    }
+    return answer;
+}
+//do range minimum query using sparse table
 #define MAX 500
  
 // lookup[i][j] is going to store minimum value in arr[i..j]. Ideally lookup table
@@ -83,11 +115,14 @@ int query(int L, int R)
 }
 int main()
 {
-    int a[] = { 7, 2, 3, 0, 5, 10, 3, 12, 18 };
-    int n = sizeof(a) / sizeof(a[0]);
-    buildSparseTable(a, n);
-    cout << query(0, 4) << endl;
-    cout << query(4, 7) << endl;
-    cout << query(7, 8) << endl;
+    int arr[] = { 3, 7, 2, 5, 8, 9 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    buildSparseTable(arr, n);
+ 
+    cout << query(0, 5) << endl;
+    cout << query(3, 5) << endl;
+    cout << query(2, 4) << endl;
+ 
     return 0;
 }
