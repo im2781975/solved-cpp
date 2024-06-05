3d720a04b0461da888776866a2e1773bf439d068
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-05 15:54:50 +0200

    Update Find largest.cpp

--- Array/Impletion/Find largest.cpp
+++ Array/Impletion/Find largest.cpp
@@ -49,6 +49,35 @@ void find3largest(int arr[], int n)
             break;
     }
 }
+void print2largest(int arr[], int n) {
+    if (n < 2) {
+        cout << "Invalid Input";
+        return;
+    }
+    sort(arr, arr + n);
+    // Start from the second last element as the largest element is at the last
+    for (int i = n - 2; i >= 0; i--) {
+        
+        if (arr[i] != arr[n - 1]) {
+            cout << "\nThe second largest element is " << arr[i];
+            return;
+        }
+    }
+    cout << "\nThere is no second largest element ";
+}
+void print2ndlargest(vector<int> vec) {
+    set<int>s(vec.begin(), vec.end());
+    vec.clear();
+    // Insert all elements back into the vector in sorted order.
+    set<int>::iterator it;
+    for(it=s.begin(); it!=s.end(); it++)
+    {
+        vec.push_back(*it);
+    }
+    cout << "\nThe Second Largest Element in Vector is: ";
+    cout<<vec[vec.size()-1];
+    print2ndlargest(vec);
+}
 int main() 
 { 
     int arr[] = { 12, 13, 1, 10, 34, 11, 34 }; 
@@ -56,6 +85,7 @@ int main()
      
     print3largest(arr, n); 
     cout<<"\nlargest element are: ";
+    vector<int> vec(arr, arr + n);
     find3largest(arr, n);
     return 0; 
 } 
