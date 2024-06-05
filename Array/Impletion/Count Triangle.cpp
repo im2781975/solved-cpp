46998fa9036991cc860b6bda239c892cf56fc9a9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-05 14:28:08 +0200

    Update and rename Count triangle.cpp to Count Triangle.cpp

--- Array/Impletion/Count Triangle.cpp
+++ Array/Impletion/Count Triangle.cpp
@@ -1,11 +1,8 @@
-// number of possible triangles using brute force approach
 #include <bits/stdc++.h>
 using namespace std;
-
-//to count all possible triangles 
+//count all possible triangles 
 int findNumberOfTriangles(int arr[], int n)
 {
-    // Count of triangles
     int count = 0;
     // The three loops select three different values from array
     for (int i = 0; i < n; i++) {
@@ -23,19 +20,15 @@ int findNumberOfTriangles(int arr[], int n)
 int findNumberOf_Triangles(int arr[], int n)
 {
     sort(arr, arr + n);
-    // Initialize count of triangles
     int count = 0;
-    // Fix the first element.need to run till n-3 as the other two elements are selected from arr[i+1...n-1]
-    for (int i = 0; i < n - 2; ++i) {
+    for (int i = 0; i < n - 2; ++i){
         //index of the rightmost third element
         int k = i + 2;
-        // Fix the second element
         for (int j = i + 1; j < n; ++j) {
             // Find the rightmost element which is smaller than the sum of two fixed elements. 
             // If value of arr[i] +arr[j-1] was greater than arr[k], then arr[i] + arr[j] must be greater than k, because the array is sorted.
             while (k < n && arr[i] + arr[j] > arr[k])
                 ++k;
- 
             // Total number of possible triangles that can be formed with the two fixed elements is
             // k - j - 1. The two fixed elements are arr[i] & arr[j]. All elements between arr[j+1]/ to
             // arr[k-1] can form a triangle with arr[i] & arr[j]. One is subtracted from k because k is
@@ -47,6 +40,28 @@ int findNumberOf_Triangles(int arr[], int n)
     }
     return count;
 }
+void CountTriangles(vector<int> A)
+{
+    int n = A.size();
+    sort(A.begin(), A.end());
+ 
+    int count = 0;
+    for (int i = n - 1; i >= 1; i--) {
+        int l = 0, r = i - 1;
+        while (l < r) {
+            if (A[l] + A[r] > A[i]) {
+ 
+                // If it is possible with a[l], a[r] & a[i] then  also possible with a[l+1]..a[r-1], a[r] and a[i]
+                count += r - l;
+                r--;
+            }
+            else
+            // if not possible check for higher values of arr[l]
+                l++;
+        }
+    }
+    cout << "No of possible solutions: " << count;
+}
 int main()
 {
     int arr[] = { 10, 21, 22, 100, 101, 200, 300 };
@@ -57,5 +72,7 @@ int main()
          
     cout << "\nTotal number of triangles possible is "
          << findNumberOf_Triangles(arr, size);
+    vector<int> A = { 10, 21, 22, 100, 101, 200, 300 };
+    CountTriangles(A);
     return 0;
 }
