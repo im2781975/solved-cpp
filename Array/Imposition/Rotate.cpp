8f8b94d976892fe8d5fea7abd1c6cf22b1183167
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-05 20:04:33 +0200

    Update Rotate.cpp

--- Array/Imposition/Rotate.cpp
+++ Array/Imposition/Rotate.cpp
@@ -37,17 +37,59 @@ void leftRotation(int arr[], int k, int n)
         {cout << arr[i] << " ";}
     cout << "\n";
 }
+// Function to rotate an array by k elements to the right
+void rotateArray(vector<int>& arr, int k)
+{
+    int n = arr.size();
+ 
+    // Mod k with the size of the array To handle the case where k is greater than the size of the array
+    k %= n;
+    reverse(arr.begin(), arr.end());
+    // Reverse the first k elements
+    reverse(arr.begin(), arr.begin() + k);
+    // Reverse the remaining n-k elements
+    reverse(arr.begin() + k, arr.end());
+}
+// reverse arr[] from index start to end
+void reverseArray(int arr[], int start, int end)
+{
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+// Function to left rotate arr[] of size n by d
+void leftRotate(int arr[], int d, int n)
+{
+    if (d == 0)
+        return;
+    // In case the rotating factor is greater than array length
+    d = d % n;
+    reverseArray(arr, 0, d - 1);
+    reverseArray(arr, d, n - 1);
+    reverseArray(arr, 0, n - 1);
+}
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+}
 int main()
 {
     int arr[] = { 1, 3, 5, 7, 9 };
     int n = sizeof(arr) / sizeof(arr[0]);
     int k = 2;
     leftRotate(arr, n, k);
+
+    vector<int> arr = { 1, 2, 3, 4, 5 };
+    int k = 2;
+    rotateArray(arr, k);
  
-    k = 3;
-    left_Rotate(arr, n, k);
- 
-    k = 4;
-    leftRotation(arr, n, k);
+    for (int i : arr) {
+        cout << i << " ";
+    }
     return 0;
 }
