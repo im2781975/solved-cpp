0e72b72bfb84cac2a00a10c52809b52cb497e37b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-05 20:08:36 +0200

    Update Shift zero.cpp

--- Array/Imposition/Shift zero.cpp
+++ Array/Imposition/Shift zero.cpp
@@ -30,5 +30,43 @@ int main()
 { 
     vector<int> m{ 5, 6, 0, 4, 6, 0, 9, 0, 8 }; 
     move_zeros_to_right(m); 
+    int A[] = { 5, 6, 0, 4, 6, 0, 9, 0, 8 }; 
+    int n = sizeof(A) / sizeof(A[0]); 
+    int B[n]; 
+    int j = 0; 
+    int count = 0; 
+    for (int i = 0; i < n; i++) { 
+        if (A[i] != 0) { 
+            B[j] = A[i]; 
+            j++; 
+        } 
+        else { 
+            count++; 
+        } 
+    } 
+    while (count > 0) { 
+        B[j] = 0; 
+        count--; 
+        j++; 
+    } 
+    for (int i = 0; i < n; i++) { 
+        A[i] = B[i]; 
+    } 
+  
+    for (int i = 0; i < n; i++) { 
+        cout << A[i] << " "; 
+    }
+    int A[] = { 5, 6, 0, 4, 6, 0, 9, 0, 8 }; 
+    int n = sizeof(A) / sizeof(A[0]); 
+    int j = 0; 
+    for (int i = 0; i < n; i++) { 
+        if (A[i] != 0) { 
+            swap(A[j], A[i]); 
+            j++; 
+        } 
+    } 
+    for (int i = 0; i < n; i++) { 
+        cout << A[i] << " ";
+    } 
     return 0; 
-} 
+}
