59705b82443c2ed2226e845322e925420b0c59f4
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-05 19:27:21 +0200

    Update Find missing.cpp

--- Array/Imposition/Find missing.cpp
+++ Array/Imposition/Find missing.cpp
@@ -29,6 +29,34 @@ int findFirst_Missing(int arr[],int n ,int m)
       if(vec[i]==0){return i;} 
   } 
 } 
+int findFirstMissing(vector<int> arr , int start ,int  end,int first) 
+{ 
+    if (start < end) 
+  { 
+    int mid = (start + end) / 2; 
+  
+    // Index matches with value at that index, means missing element cannot be upto that programme.
+    if (arr[mid] != mid+first) 
+      return findFirstMissing(arr, start,mid , first); 
+    else
+      return findFirstMissing(arr, mid + 1, end , first); 
+  } 
+  return start + first; 
+  
+} 
+int findSmallestMissinginSortedArray(vector<int> arr) 
+{ 
+    // Check if 0 is missing in the array 
+    if(arr[0] != 0) 
+    return 0; 
+  
+  // Check is all numbers 0 to n - 1 are present in array 
+    if(arr[arr.size() - 1] == arr.size() - 1) 
+    return arr.size(); 
+  
+    int first = arr[0]; 
+    return findFirstMissing(arr, 0, arr.size() - 1, first); 
+} 
 int main() 
 { 
     int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 10}; 
@@ -38,4 +66,7 @@ int main()
     int m=6;
     cout << "Smallest missing element is " << 
         findFirst_Missing(arr, 0, m) << endl; 
+    vector<int> arr = {0, 1, 2, 3, 4, 5, 7}; 
+    int n = arr.size(); 
+    cout<<"First Missing element is : "<<findSmallestMissinginSortedArray(arr); 
 } 
