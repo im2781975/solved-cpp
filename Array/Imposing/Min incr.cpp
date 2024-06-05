4d970cc9d47d89b734f876f8a2b9e1ecc2079e84
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-05 17:20:50 +0200

    Update Min incr.cpp

--- Array/Imposing/Min incr.cpp
+++ Array/Imposing/Min incr.cpp
@@ -1,7 +1,24 @@
 // Program to make all array equal
 #include <bits/stdc++.h>
 using namespace std;
- 
+bool isPairSum(int A[], int N, int X)
+{
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            // as equal i and j means same element
+            if (i == j)
+                continue;
+            // pair exists
+            if (A[i] + A[j] == X)
+                return true;
+            // as the array is sorted
+            if (A[i] + A[j] > X)
+                break;
+        }
+    }
+    //doesn't find any pair..
+    return false;
+}
 // function for calculating min operations
 int minOps(int arr[], int n, int k)
 {
@@ -25,13 +42,16 @@ int minOps(int arr[], int n, int k)
     // return result
     return res;
 }
- 
-// driver program
 int main()
 {
     int arr[] = { 21, 33, 9, 45, 63 };
     int n = sizeof(arr) / sizeof(arr[0]);
     int k = 6;
     cout << minOps(arr, n, k);
+    int ar[] = { 2, 3, 5, 8, 9, 10, 11 };
+    int val = 17;
+    int arrSize = *(&ar + 1) - ar;
+    sort(ar, ar + arrSize);
+    cout << isPairSum(ar, arrSize, val);
     return 0;
 }
