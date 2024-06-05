fbbcb644da506f947681790fd141ced8a178b6fb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-05 16:48:24 +0200

    Update Recursive.cpp

--- Array/Impletion/Recursive.cpp
+++ Array/Impletion/Recursive.cpp
@@ -14,6 +14,34 @@ int sumArray(int arr[], int size) {
         return arr[size - 1] + sumArray(arr, size - 1);
     }
 }
+void revereseArray(int arr[], int start, int end) 
+{ 
+    while (start < end) 
+    { 
+        int temp = arr[start];  
+        arr[start] = arr[end]; 
+        arr[end] = temp; 
+        start++; 
+        end--; 
+    }  
+}      
+void rvereseArray(int arr[], int start, int end) 
+{ 
+    if (start >= end) 
+    return; 
+      
+    int temp = arr[start];  
+    arr[start] = arr[end]; 
+    arr[end] = temp; 
+      
+    // Recursive Function calling 
+    rvereseArray(arr, start + 1, end - 1);  
+}      
+void printArray(int arr[], int size) 
+{ 
+   for (int i = 0; i < size; i++) 
+   cout << arr[i] << " "; 
+}  
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
     int size = sizeof(arr) / sizeof(arr[0]);
@@ -22,6 +50,8 @@ int main() {
     int res = MultArray(arr, size);
     cout << "\nThe sum of the elements in the array is: " << result ;
     cout << "\nThe mult of the elements in the array is: " << res ;
+    revereseArray(arr, 0, n);
+    cout <<"\nReversed array is: ";
+    printArray(arr, n); 
     return 0;
 }
-
