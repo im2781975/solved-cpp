6afd31fa5b4ba203ab4e197990b3e626da2a7e82
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-05 17:15:19 +0200

    Update Find peak.cpp

--- Array/Imposing/Find peak.cpp
+++ Array/Imposing/Find peak.cpp
@@ -1,6 +1,29 @@
 // find a peak element 
 #include <bits/stdc++.h> 
 using namespace std; 
+int findpeak(int arr[], int n) 
+{ 
+    int l = 0; 
+    int r = n-1; 
+    int mid; 
+    
+    while (l <= r) { 
+        // finding mid by binary right shifting. 
+        mid = (l + r) >> 1; 
+//midpoint is calculated by right-shifting the sum of l and r by 1 (which is equivalent to dividing by 2)
+        // first case if mid is the answer 
+        if ((mid == 0 || arr[mid - 1] <= arr[mid]) 
+            and (mid == n - 1 || arr[mid + 1] <= arr[mid])) 
+            break; 
+        //If mid is a peak (greater than or equal to its neighbors or it is at the boundary), the loop breaks.
+        
+        if (mid > 0 and arr[mid - 1] > arr[mid]) 
+            r = mid - 1; 
+        else
+            l = mid + 1; 
+    } 
+    return mid; 
+} 
 int findPeak(int arr[], int n) 
 { 
     // first or last element is peak element 
@@ -51,5 +74,6 @@ int main()
     int arr[]{ 1, 3, 20, 4, 1, 0 }; 
     int n = sizeof(arr) / sizeof(arr[0]); 
     cout << "Index of a peak point is " << findPeak(arr, n); 
+    cout << "Index of a peak point is " << findpeak(arr, N); 
     return 0; 
 } 
