16dafdac000edfcab93dd7c0897de09d8ff5a437
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-05 19:44:45 +0200

    Update Max profit.cpp

--- Array/Imposition/Max profit.cpp
+++ Array/Imposition/Max profit.cpp
@@ -51,12 +51,71 @@ int maxtwobuysell(int arr[],int size) {
     }
      return second_sell;
 }
+//----
+int f(int idx, int buy, int prices[], int cap, int n)
+{
+    if (cap == 0) {
+        return 0;
+    }
+    if (idx == n) {
+        return 0;
+    }
+    int profit = 0;
+    // you can either buy or not buy
+    if (buy == 0) {
+      profit = max(-prices[idx]
+     + f(idx + 1, 1, prices,  cap, n),f(idx + 1, 0, prices,  cap, n));
+    }
+    // you can either sell or not sell
+    else {
+         profit = max( prices[idx]
+        + f(idx + 1, 0, prices, cap - 1, n),f(idx + 1, 1, prices,  cap, n));
+    }
+    return profit;
+}
+int maxtwobuysell(int prices[], int n)
+{
+    return f(0, 0, prices, 2, n);
+}
+//-----
+int f(int idx, int buy, int prices[],vector<vector<vector<int> > >& dp, int cap, int n)
+{
+    if (cap == 0) {
+        return 0;
+    }
+    if (idx == n) {
+        return 0;
+    }
+    if (dp[idx][buy][cap] != -1) {
+        return dp[idx][buy][cap];
+    }
+    int profit = 0;
+    // you can either buy or not buy
+    if (buy == 0) {
+        dp[idx][buy][cap] = profit
+            = max(-prices[idx]
+         + f(idx + 1, 1, prices, dp, cap, n),f(idx + 1, 0, prices, dp, cap, n));
+    }
+    // you can either sell or not sell
+    else {
+        dp[idx][buy][cap] = profit = max (prices[idx]
+         + f(idx + 1, 0, prices, dp, cap - 1, n),f(idx + 1, 1, prices, dp, cap, n));
+    }
+ 
+    return profit;
+}
+int maxtwobuysell(int prices[], int n)
+{
+    vector<vector<vector<int> > > dp( n, vector<vector<int> >(2, vector<int>(3, -1)));
+    return f(0, 0, prices, dp, 2, n);
+}
 int main()
 {
     int price[] = { 2, 30, 15, 10, 8, 25, 80 };
     int n = sizeof(price) / sizeof(price[0]);
     cout << "Maximum Profit = " << maxProfit(price, n);
     cout << "\nMaximum Profit = " << maxtwobuysell(price, n);
-    
+    cout << maxtwobuysell(price, n);
+    cout << maxtwobuysell(price, n);
     return 0;
 }
