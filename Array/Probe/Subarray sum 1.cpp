374d06f46beaa0b898b2ea6edc546a2e4a849733
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-06 20:55:03 +0200

    Update Subarray sum 1.cpp

--- Array/Probe/Subarray sum 1.cpp
+++ Array/Probe/Subarray sum 1.cpp
@@ -3,28 +3,27 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-// The main function to find the K-th largest sum of contiguous subarray using Prefix Sum and Sorting approach.
+// find the K-th largest sum of contiguous subarray using Prefix Sum and Sorting approach
 int kthLargestSum(vector<int>& arr, int k)
 {
     int n = arr.size();
-    vector<int> prefixSum(n + 1);
-    prefixSum[0] = 0;
+    vector<int> Sum(n + 1);
+    Sum[0] = 0;
     for (int i = 1; i <= n; i++) {
-        prefixSum[i] = prefixSum[i - 1] + arr[i - 1];
+        Sum[i] = Sum[i - 1] + arr[i - 1];
     }
     // Create a vector to store all possible subarray sums.
-    vector<int> subarraySums;
+    vector<int> vec;
     for (int i = 0; i <= n; i++) {
         for (int j = i + 1; j <= n; j++) {
-            subarraySums.push_back(prefixSum[j]- prefixSum[i]);
+            vec.push_back(Sum[j]- Sum[i]);
         }
     }
-
     // Sort the subarray sums in decreasing order.
-    sort(subarraySums.begin(), subarraySums.end(),
+    sort(Sums.begin(), Sums.end(),
          greater<int>());
     // Return the K-th largest sum of contiguous subarray.
-    return subarraySums[k - 1];
+    return Sums[k - 1];
 }
 int main()
 {
