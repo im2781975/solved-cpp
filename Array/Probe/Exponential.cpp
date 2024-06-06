2219b9a52eefe9aab8e74d070549f2df41d8adb0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-06 07:52:22 +0200

    Update Exponential.cpp

--- Array/Probe/Exponential.cpp
+++ Array/Probe/Exponential.cpp
@@ -40,6 +40,31 @@ int binarySearch(int arr[], int l, int r, int x)
     }
     return -1;
 }
+int exponential_search(vector<int> arr,int x){
+    int n = arr.size();
+     
+    if(n == 0)
+        return -1;
+ 
+    // Find range for binary search by repeatedly doubling i
+    int i = 1;
+    while(i < n and arr[i] < x)
+        i *= 2;
+ 
+    // Perform binary search on the range [i/2, min(i, n-1)]
+    int left = i /2;
+    int right = min(i, n-1);
+ 
+    while(left <= right){
+        int mid = (left + right)/2;
+         
+        if(arr[mid] == x) return mid;
+        else if(arr[mid] < x) left = mid + 1;
+        else right = mid - 1;
+    }
+ 
+    return -1;
+}
 int main(void)
 {
    int arr[] = {2, 3, 4, 10, 40};
@@ -48,5 +73,17 @@ int main(void)
    int result = exponentialSearch(arr, n, x);
    (result == -1)? cout <<"Element is not present in array"
         : cout <<"Element is present at index " << result;
+        
+    vector<int> arr{2, 3, 4, 10, 40};
+    int n = arr.size();
+    int x = 10;
+    int result = exponential_search(arr, x);
+     
+    if(result == -1){
+        cout << "Element not found in the array";
+    }
+    else{
+        cout << "Element is present at index " << result << endl;
+    }
    return 0;
 }
