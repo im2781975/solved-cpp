cc03a7fde321412e1b83d088d33425f99fdf406f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-06 21:20:26 +0200

    Update Jump.cpp

--- Array/Probe/Jump.cpp
+++ Array/Probe/Jump.cpp
@@ -9,11 +9,6 @@ int jumpSearch(int arr[], int x, int n)
     int prev = 0;
     while (arr[min(step, n)-1] < x)
     {
-        // arr[min(step, n)-1] is equivalent to arr[min(4, 16)-1] which is arr[3] (since indexing starts from 0).
-        //arr[3] is 2, and x is 55, so the condition arr[min(step, n)-1] < x is true.
-        //Set prev = step (prev becomes 4).
-        //Increment step by sqrt(n), so step becomes 8.
-        //Check if prev is greater than or equal to n (16). Since it's not, continue to the next iteration.
         prev = step;
         step += sqrt(n);
         if (prev >= n)
@@ -37,10 +32,7 @@ int main()
     int arr[] = { 0, 1, 1, 2, 3, 5, 8, 13, 21,34, 55, 89, 144, 233, 377, 610 };
     int x = 55;
     int n = sizeof(arr) / sizeof(arr[0]);
-    // Find the index of 'x' using Jump Search
     int index = jumpSearch(arr, x, n);
-    // Print the index where 'x' is located
     cout << "\nNumber " << x << " is at index " << index;
     return 0;
 }
-
