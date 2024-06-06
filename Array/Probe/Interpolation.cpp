8e1cdf8576c708958fcd285d450c0f50e7811e10
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-06 21:24:39 +0200

    Update Interpolation.cpp

--- Array/Probe/Interpolation.cpp
+++ Array/Probe/Interpolation.cpp
@@ -1,39 +1,27 @@
 //interpolation search 
 #include <bits/stdc++.h>
 using namespace std;
-// If x is present in arr[], then returns index of it, else returns -1.
 //recursive approch
 int interpolationSearch(int arr[], int lo, int hi, int x)
 {
     int pos;
- 
     // Since array is sorted, an element present in array must be in range defined by corner
     if (lo <= hi && x >= arr[lo] && x <= arr[hi]) {
- 
         // Probing the position with keeping uniform distribution in mind.
         pos = lo+ (((double)(hi - lo) / (arr[hi] - arr[lo]))* (x - arr[lo]));
-        // int pos=0+(((double)(14-0)/(arr[14]-arr[0]))*(18-arr[0]));
-        // int pos=0+(((double)(14-0)/(47-10)*(18-10));
-        // int pos=(double)14/(37*8);
-        
         if (arr[pos] == x)
             return pos;
-        // If x is larger, x is in right sub array
         if (arr[pos] < x)
-        return interpolationSearch(arr, pos + 1, hi, x);
- 
-        // If x is smaller, x is in left sub array
+            return interpolationSearch(arr, pos + 1, hi, x);
         if (arr[pos] > x)
-        return interpolationSearch(arr, lo, pos - 1, x);
+            return interpolationSearch(arr, lo, pos - 1, x);
     }
     return -1;
 }
 //iterative approch
 int interpolationSearch(int arr[], int n, int x)
 {
-    // Find indexes of two corners
     int low = 0, high = (n - 1);
-    // Since array is sorted, an element present in array must be in range defined by corner
     while (low <= high && x >= arr[low] && x <= arr[high])
     {
         if (low == high)
@@ -44,15 +32,10 @@ int interpolationSearch(int arr[], int n, int x)
         int pos = low + (((double)(high - low) /
             (arr[high] - arr[low])) * (x - arr[low]));
    
-        // int pos=0+(((double)(14-0)/(arr[14]-arr[0]))*(18-arr[0]));
-        // int pos=0+(((double)(14-0)/(47-10)*(18-10));
-        // int pos=(double)14/(37*8);
         if (arr[pos] == x)
             return pos;
-        // If x is larger, x is in upper part
         if (arr[pos] < x)
             low = pos + 1;
-        // If x is smaller, x is in the lower part
         else
             high = pos - 1;
     }
@@ -65,11 +48,9 @@ int main()
 
     int x = 18;
     int index = interpolationSearch(arr, 0, n - 1, x);
- 
     if (index != -1)
         cout << "\nElement found at index " << index;
     else
         cout << "\nElement not found.";
- 
     return 0;
 }
