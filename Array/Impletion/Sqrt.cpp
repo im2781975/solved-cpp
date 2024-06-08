fa9108abbcb54bfc7aafe09a097826a247ea5b76
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-08 11:19:43 +0200

    Update and rename Sqrt.cpp to Sqrt.cpp

--- Array/Impletion/Sqrt.cpp
+++ Array/Impletion/Sqrt.cpp
@@ -1,12 +1,8 @@
-// C++ program to demonstrate working of simple
-// array query using iteration
+// working of simple array query using iteration
 #include <bits/stdc++.h>
 using namespace std;
- 
 #define MAXN 10000
- 
-int arr[MAXN]; // original array
- 
+int arr[MAXN];
 // Time Complexity: O(r-l+1)
 int query(int l, int r)
 {
@@ -16,23 +12,16 @@ int query(int l, int r)
     }
     return sum;
 }
- 
-// Driver code
 int main()
 {
-    // We have used separate array for input because
-    // the purpose of this code is to explain simple
-    // array query using iteration in competitive
-    // programming where we have multiple inputs.
     int input[] = { 1, 5, 2, 4, 6, 1, 3, 5, 7, 10 };
     int n = sizeof(input) / sizeof(input[0]);
- 
     // copying input[] to arr[]
     memcpy(arr, input, sizeof(input));
- 
     cout << "query(3,8) : " << query(3, 8) << endl;
     cout << "query(1,6) : " << query(1, 6) << endl;
-    arr[8] = 0; // updating arr[8] to 0
+    arr[8] = 0;
+    // updating arr[8] to 0
     cout << "query(8,8) : " << query(8, 8) << endl;
     return 0;
 }
