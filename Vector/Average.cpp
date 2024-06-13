0ba3464a16f0d6b0db624f1b9bf009663f3029a1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-13 08:08:44 +0200

    Update Average.cpp

--- Vector/Average.cpp
+++ Vector/Average.cpp
@@ -1,21 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-float getAverage(float* arr, int size)
+int getavg(int *arr, int n)
 {
     int sum = 0;
-    for (int i = 0; i < size; i++){
+    for(int i = 0; i < n; i++)
         sum += arr[i];
-    }
-    return sum / size;
+    float avg = sum/n;
+    return avg;
 }
 int main()
 {
-    float arr[5]{ 10, 20, 30, 40, 50 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Array Elements: ");
-    for (int i = 0; i < n; i++) {
-        printf("%.0f ", arr[i]);
-    }
-    cout<< "\nAverage is: "<< getAverage(arr,n) ;
-    return 0;
+    int arr[]{2, 3, 1, 5, 4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << getavg(arr, n);
 }
