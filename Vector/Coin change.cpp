9ed6f2297b2a82ff5c0536dc5debddc3a062e844
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-13 10:49:02 +0200

    Update Coin change.cpp

--- Vector/Coin change.cpp
+++ Vector/Coin change.cpp
@@ -1,22 +1,20 @@
 //how much coin need for complete n
 #include<bits/stdc++.h>
 using namespace std;
-vector<int>coins={1,3,4};
+vector<int>coins{3, 4, 5};
 int coin_change(int n)
 {
-    if(n==0)
-    return 0;
-    int ans=1000;
-    for(int i=0;i<coins.size();i++)
-    {
-        if(n>=coins[i])
-            ans=min(ans,1+coin_change(n-coins[i]));
-    }
+    int mini = *min_element(coins.begin(), coins.end());
+    if(n < mini)
+         return 0;
+    int ans = 10000;
+    for(int i = 0; i < coins.size() -1 ; i++)
+        ans = min(ans, 1 + coin_change(n - coins[i]));
     return ans;
 }
 int main()
 {
     int n;
-    cin>>n;
-    cout<<coin_change(n);
+    cin >> n;
+    cout << coin_change(n);
 }
