e7aac91e87b8bf1cb341128040b2d74299198d04
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-13 19:01:35 +0200

    Update Distinct count.cpp

--- Vector/Distinct count.cpp
+++ Vector/Distinct count.cpp
@@ -1,26 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int i, ans = 0;
-    int n;
-    cin >> n; 
-    vector<int> arr;
-    arr.resize(n);
-
-    for (i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    sort(arr.begin(), arr.end());
-
-    for (i = 0; i < n; i++) {
-        if (i == 0) {
+int main()
+{
+    int n, ans = 0;
+    cin >> n;
+    vector<int>vec;
+    vec.resize(n);
+    for(int i = 0; i < n; i++)
+        cin >> vec[i];
+    sort(vec.begin(), vec.end());
+    for(int i = 1; i <= n; i++)
+    {
+        if(i == 0)
+        {
             ans++;
             continue;
         }
-        if (arr[i] != arr[i - 1])
+        if(vec[i]!=vec[i - 1])
             ans++;
     }
-    cout << ans << "\n";
-    return 0;
+    cout << ans;
 }
