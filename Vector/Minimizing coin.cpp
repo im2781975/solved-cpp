a3b8466a91aeec671dd241645207de4196672795
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-13 20:48:15 +0200

    Update Minimizing coin.cpp

--- Vector/Minimizing coin.cpp
+++ Vector/Minimizing coin.cpp
@@ -20,3 +20,24 @@ int main()
     }
     cout << NumberOfCoin[x];
 }
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+    vector<int>coin(n);
+    vector<int>cnt(x + 1, INT_MAX);
+    for(int i = 0; i < n; i++)
+        cin >> coin[i];
+        
+    for(int i = 0; i <= x; i++)
+    {
+        for(int j = 0; j < coin.size(); j++)
+        {
+            if(i - coin[j] >=0)
+                cnt[i] = min(cnt[i], cnt[i - coin[j] + 1]);
+        }
+    }
+    cout << cnt[x];
+}
