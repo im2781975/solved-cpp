59ab08b2c4654c2f9d9242862070d49d536f7a25
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-19 05:25:45 +0200

    Update Vector.cpp

--- Practice/Vector.cpp
+++ Practice/Vector.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 int main()
 {
-    vector<int>vect{10, 20, 30, 40 };
-    for (int& x : vect)
-        x+=5;
-    for (int x : vect)
+    vector <int> vec{1, 2, 3, 4, 5};
+    for(auto &x : vec)
+        x += 5;
+    for(int x : vec)
         cout << x << " ";
-        
-    cout << '\n';
-    return 0;
+    cout << "\n";
+    vector <int> cev(5, 3);
+    for(int i = 0; i < 5 - 3; i++)
+        cev[i] = i;
+    for(int i = 0; i < cev.size(); i++)
+        cout << cev[i] << " ";
+    cout << "\n";
+    vector <int> v(5, 0);
+    v[0] = -1;
+    v.resize(10, 10);
+    cout << v.front() << "\n";
+    cout << v[7];
 }
