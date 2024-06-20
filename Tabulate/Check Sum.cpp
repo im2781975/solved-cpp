f9d2f9071370bce5b2c86fad2e316ad06a1320a3
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-20 05:21:15 +0200

    Update Check Sum.cpp

--- Tabulate/Check Sum.cpp
+++ Tabulate/Check Sum.cpp
@@ -1,22 +1,17 @@
-//check if there exist two distinct elements in the sorted array whose sum is equal to 'k'. If such elements exist, output "Yes"; otherwise, output "No".
+//check if there exist two distinct elements in the sorted array whose sum is equal to 'k'.
+//If such elements exist, output "Yes"; otherwise, output "No".
 #include<bits/stdc++.h>
 using namespace std;
-
 bool found = false;
 int k;
-
 vector<int> merge(vector<int> a)
 {
     int n = a.size();
-    
     if (a.size() <= 1)
         return a;
     
-    vector<int> b;
-    vector<int> c;
-    
+    vector<int> b, c;
     int mid = a.size() / 2;
-    
     for (int i = 0; i < mid; i++)
         b.push_back(a[i]);
     
@@ -26,7 +21,6 @@ vector<int> merge(vector<int> a)
     vector<int> sorted_b = merge(b);
     vector<int> sorted_c = merge(c);
     vector<int> sorted_a;
-    
     int idx1 = 0, idx2 = 0;
     
     for (int i = 0; i < a.size(); i++)
@@ -52,21 +46,16 @@ vector<int> merge(vector<int> a)
             idx2++;
         }
     }
-    
     if (sorted_a.size() == n)
     {
         int i = 0, j = n - 1;
-        
         if (sorted_a[i] + sorted_a[j] > k)
             j--;
         else if (sorted_a[i] + sorted_a[j] < k)
             i++;
         else
-        {
             found = true;
-        }
     }
-    
     return sorted_a;
 }
 void print(vector<int> res)
@@ -79,19 +68,15 @@ int main()
     int n;
     cin >> n;
     int arr[n + 2];
-    
     for (int i = 0; i < n; i++)
         cin >> arr[i];
     
     cin >> k;
-    
     vector<int> ar(arr, arr + n);
     vector<int> ans = merge(ar);
-    
     if (found == true)
         cout << "Yes";
     else
         cout << "No";
-        
     return 0;
 }
