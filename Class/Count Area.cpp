66bf5c7ee2dca5316c6a1f73bc51d0b5ba6929b7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-28 16:36:34 +0200

    Update Count Area.cpp

--- Class/Count Area.cpp
+++ Class/Count Area.cpp
@@ -1,27 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-class construct
-{ 
-    public:
-    float area; 
-    construct()
-    {
+class construct{
+    public: float area;
+    construct(){
         area = 0;
     }
-    construct(int a, int b)
-    {
-        area = a * b;
+    construct(int a, int b){
+        area =a*b;
     }
-    void disp()
-    {
-        cout<< area<<"\n";
+    void disp(){
+        cout << "\nArea is: " << area;
     }
 };
-int main()
-{
-    construct c;
-    construct c2( 10, 20);
-      
+int main(){
+    construct c, c2(10, 20);
     c.disp();
     c2.disp();
 }
