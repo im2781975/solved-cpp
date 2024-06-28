13ac92b9dab2f4a7f1c67a9d81caadc3c9134783
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-28 16:42:10 +0200

    Update Overload 1.cpp

--- Class/Overload 1.cpp
+++ Class/Overload 1.cpp
@@ -1,23 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-class Test {
-    protected:
-    int x;
- 
+class test{
+    protected: int x;
     public:
-    Test(int i): x(i){}
-    void fun() const
-    {
-        cout << "fun() const called " << "\n";
+    test(int i):x(i){}
+    void fun()const{
+        cout << "\nconst called";
     }
-    void fun()
-    { cout << "fun() called " << "\n"; }
+    void fun(){
+        cout <<"\nFun called";
+    }
 };
-int main()
-{
-    const Test t2(20);
-    Test t1(10);
-    t2.fun();
-    t1.fun();
-    return 0;
+int main(){
+    test a(20);
+    const test b= 30;
+    a.fun();
+    b.fun();
 }
