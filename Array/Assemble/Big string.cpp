80c10b368356ab2e72a7c2c13029db8439814679
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-12 14:17:16 +0100

    Update Big string.cpp

--- Array/Assemble/Big string.cpp
+++ Array/Assemble/Big string.cpp
@@ -1,33 +1,17 @@
-//sort the Big integers in ascending order 
-#include<bits/stdc++.h> 
-using namespace std; 
- 
-bool comp(const string &left, const string &right) 
-{ 
-    // if length of both string are equals then sort them in lexicographically order 
-    if (left.size() == right.size()) 
-        return left < right; 
-  
-    // Otherwise sort them according to the length  of string in ascending order 
-    else
-        return left.size() < right.size(); 
-} 
-// Function to sort arr[] elements according to integer value 
-void SortingBigIntegers(string arr[], int n) 
-{ 
-    vector<string> sortArr(arr, arr + n); 
-  
-    sort(sortArr.begin(), sortArr.end(), comp); 
- 
-    for (auto &ele : sortArr) 
-        cout << ele << " "; 
-} 
-int main() 
-{ 
+#include<bits/stdc++.h>
+using namespace std;
+bool comp(const string &l, const string &r){
+    if(l.size() == r.size()) return l < r;
+    else return l.size() < r.size();
+}
+void ingsort(string *arr, int n) {
+    vector <string> vec(arr, arr + n);
+    sort(vec.begin(), vec.end(), comp);
+    for(int i = 0; i < vec.size(); i++)
+        cout << vec[i] << " ";
+}
+int main(){
     string arr[] = {"54", "724523015759812365462", "870112101220845", "8723"};
-    int n = sizeof(arr) / sizeof(arr[0]); 
-  
-    SortingBigIntegers(arr, n); 
-  
-    return 0; 
-} 
+    int n = sizeof(arr) / sizeof(arr[0]);
+    ingsort(arr, n);
+}
