a788fcd7d98b8c5d503ea78bc37283835c9b7a9f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-15 00:19:02 +0100

    Update 1.cpp

--- Array/Probe/1.cpp
+++ Array/Probe/1.cpp
@@ -27,31 +27,9 @@ int Ternary(int *arr, int l, int r, int key){
     }
     return -1;
 }
-int poslt(int *arr, int lt, int rt, int key){
-    while(rt - lt > 1){
-        int mid = lt + (rt - lt) / 2;
-        if(arr[mid] >= key)  rt = mid;
-        else    lt = mid;
-    }
-    return rt;
-}
-int posrt(int *arr, int lt, int rt, int key){
-    while(rt - lt > 1){
-        int mid = lt + (rt - lt) / 2;
-        if(arr[mid] <= key)  lt = mid;
-        else    rt = mid;
-    }
-    return lt;
-}
-int cntfreq(int *arr, int n, int key){
-    int lt = poslt(arr, -1, n - 1, key);
-    int rt = posrt(arr, 0, n - 1, key);
-    return (arr[lt] == key && key == arr[rt]) ? (rt - lt + 1) : 0;
-}
 int main(){
     int arr[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     int n = sizeof(arr) / sizeof(arr[0]);
     int l = 0, r = n - 1, key = 4;
     cout << Ternary(arr, l, r, key);
-    cout << cntfreq(arr, n, key) << " " << key << endl;
 }
