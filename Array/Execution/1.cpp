feee1f436145717ad3689ad7373bc0298a160eaa
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-18 19:11:07 +0100

    Update 1.cpp

--- Array/Execution/1.cpp
+++ Array/Execution/1.cpp
@@ -78,6 +78,55 @@ int cntTriangle(vector <int> vec){
     }
     cout << cnt;
 }
+int findXOR(int *arr, int n) { 
+    // Do XOR of all elements and return 
+    int res = arr[0]; 
+    for (int i = 1; i < n; i++) 
+        res = res ^ arr[i]; 
+    return res; 
+} 
+//find the array element that appears only once 
+int occuronce(int *arr, int n) { 
+    for (int i = 0; i < n; i++) { 
+        int count = 0; 
+        for (int j = 0; j < n; j++) { 
+            if (arr[i] == arr[j])  
+                count++; 
+        }
+        if (count == 1) return arr[i]; 
+    } 
+    return -1; 
+}
+void occuronce(int *arr, int n){ 
+    unordered_map <int, int> mp;
+    for(int i = 0; i < n; i++) mp[arr[i]]++;
+    for(auto it = mp.begin(); it != mp.end(); it++){
+        if(it -> second == 1) cout << it -> first << " ";
+    }
+} 
+int singleNumber(int nums[],int n) { 
+    map <int, int> mp;
+    long sum1 = 0, sum2 = 0;
+    for(int i = 0; i < n; i++){
+        if(mp[nums[i]] == 0){
+            sum1 += nums[i];
+            mp[nums[i]]++;
+        }
+        sum2 += nums[i];
+    }
+    return 2 * (sum1) - sum2;
+} 
+int singleElement(int arr[], int n){ 
+    int low = 0, high = n - 2, mid; 
+    while (low <= high) { 
+        mid = (low + high) / 2; 
+        if (arr[mid] == arr[mid ^ 1]) low = mid + 1;
+        //XOR (^ 1) is used to find the adjacent element. If the middle element is equal 
+        //to its adjacent element, it means the single element is on the right side, so update the low pointer accordingly.
+        else high = mid - 1; 
+    } 
+    return arr[low]; 
+}
 int main(){
     vector <int> vec{ 3, 1, 4, 4, 5, 2, 6, 1 };
     int k = 2;
