0c380c67697838f4dece36bd62b0f62cb7a8bd1f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-01-18 17:39:44 +0100

    Update 1.cpp

--- Array/Execution/1.cpp
+++ Array/Execution/1.cpp
@@ -1,3 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
 //find k numbers with most occurrences in the given array
 void nfreq(int *arr, int n, int k){
     unordered_map <int, int> mp;
@@ -27,16 +29,54 @@ void nfreq(int *arr, int n, int k){
     sort(freq.begin(), freq.end(), cmp);
     for(int i = 0; i < k; i++) cout << freq[i].first << " ";
 }
-vector <int> nfreq(vector <int> vec, int k){
+vector <int> nfreq(vector <int> vec, int k, vector <int> res){
     unordered_map <int, int> mp;
     for(int i = 0; i < vec.size(); ++i) mp[vec[i]]++;
     vector <pair <int, int >> freq;
     for(auto it = mp.begin(); it!= mp.end(); ++it)
         freq.push_back({it -> second, it -> first});
-    nth_element(freq.begin(), freq.begin() + (freq.size() - k), freq.end());
-    transform(freq.begin() + (freq.size() - k), freq.end(), res.begin(), 
-    [](pair <int, int> p){return p.second; });
+    nth_element(freq.begin(), freq.end() - k, freq.end());
+    transform(freq.begin() - k, freq.end(), res.begin(), [](pair <int, int> vec){ return vec.second;});
     return res;
+}
+//count all possible triangles 
+int cntTriangle(int *arr, int n){
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            for(int k = j + 1; k < n; k++){
+                if (arr[i] + arr[j] > arr[k] && arr[i] + arr[k] > arr[j]
+                    && arr[k] + arr[j] > arr[i]) cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+int cntTriangle(int *arr, int n){
+    sort(arr, arr + n); int cnt = 0;
+    for(int i = 0; i < n - 2; i++){
+        int k = i + 2;
+        for(int j = i; j < n; j++){
+            while(k < n && arr[i] + arr[j] > arr[k]) k++;
+            if(k > j) cnt += k - j + 1;
+        }
+    }
+    return cnt;
+}
+int cntTriangle(vector <int> vec){
+    int n = vec.size(), cnt = 0;
+    sort(vec.begin(), vec.end());
+    for(int i = n - 1; i >= 1; i--){
+        int l = 0, r = i - 1;
+        while(l < r){
+            if(arr[l] + arr[r] > arr[i]) {
+                cnt += r - l;
+                r--;
+            }
+            else    l++;
+        }
+    }
+    cout << cnt;
 }
 int main(){
     vector <int> vec{ 3, 1, 4, 4, 5, 2, 6, 1 };
